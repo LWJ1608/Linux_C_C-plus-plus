3968a747f23f89df635a8f1c0bf81faeec5dcdf7
LWJ1608 <2194677100@qq.com>
2022-03-17 23:07:23 +0800

    Squashed commit of the following:
    commit 94720528a2521bfb8e7f1fe1decdaabcd763924d
    Author: LWJ1608 <93906402+LWJ1608@users.noreply.github.com>
    Date:   Wed Mar 16 16:29:50 2022 +0800
    
        C++
    
    commit 83af7a95af98fce9fe4673a0299a5b45a2d3f802
    Author: LWJ1608 <93906402+LWJ1608@users.noreply.github.com>
    Date:   Wed Mar 16 16:29:06 2022 +0800
    
        Delete 多态
    
    commit 5c6b9d9e07f820267a784bb013d26e42cd9eac06
    Author: LWJ1608 <93906402+LWJ1608@users.noreply.github.com>
    Date:   Wed Mar 16 16:25:27 2022 +0800
    
        C++
    
    commit 16459cf754f5067dc71b894be69f0b8ad68ae00b
    Author: LWJ1608 <2194677100@qq.com>
    Date:   Wed Mar 16 16:00:12 2022 +0800
    
        C++
    
    # Conflicts:
    #	C++基础/多态/多态.cpp

--- C++基础/多态/多态.cpp
+++ C++基础/多态/多态.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
 *@author lwj
 *修改日期 2022.3.16
@@ -47,3 +48,54 @@ int main(){
     a.fun(1, 1);//fun()是ClassA重定义的  
     return 0;  
 }
+=======
+/**
+*@author lwj
+*修改日期 2022.3.16
+*多态内容分析
+**/
+
+//1.重载和重写的区别：
+//
+//重载overload：在同一个类中，函数名相同，参数列表不同，编译器会根据这些函数的不同参数列表，将同名的函数名称做修饰，从而生成一些不同名称的预处理函数，未体现多态。
+
+//重写override：也叫覆盖，子类重新定义父类中有相同名称相同参数的虚函数，主要是在继承关系中出现的，被重写的函数必须是virtual的，重写函数的访问修饰符可以不同，尽管virtual是private的，子类中重写函数改为public,protected也可以，体现了多态。
+
+//重定义redefining：也叫隐藏，子类重新定义父类中有相同名称的非虚函数，参数列表可以相同可以不同，会覆盖其父类的方法，未体现多态。
+
+//a如果派生类的函数和基类的函数同名，但是参数不同，此时，不管有无virtual，基类的函数被隐藏。
+
+//b如果派生类的函数与基类的函数同名，并且参数也相同，但是基类函数没有vitual关键字，此时，基类的函数被隐藏。（如果有virtual就成重写了 )
+
+#include <iostream>  
+using namespace std;  
+class Base  
+{  
+private:  
+    virtual void display() { cout<<"Base display()"<<endl; }  
+    void show(){ cout<<"Base show()"<<endl; }  
+public:  
+    void exec(){ display(); show(); }  
+    void fun(string s) { cout<<"Base fun(string)"<<endl; }  
+    void fun(int a) { cout<<"Base fun(int)"<<endl; }//overload:两个fun函数在Base类的内部被重载  
+    virtual int function(){}  
+};  
+class ClassA:public Base  
+{  
+public:  
+    void display() { cout<<"ClassA display()"<<endl; }//override:基类中display为虚函数，且参数列表一直，故此处为重写  
+    void fun(int a,int b) { cout<<"ClassA fun(int,int)"<<endl; }//redefining:fun函数在Base类中不为虚函数，故此处为重定义  
+    void show() { cout<<"ClassA show()"<<endl; }//redefining:理由同上  
+    int function(int a){}//overload:注意这是重载而不是重写，因为参数列表不同，在编译时ClassA中其实还有个编译器自己偷偷加上的从Base继承来的int function(){};  
+};  
+int main(){  
+    ClassA a;  
+    Base *base=&a;  
+    base->exec();//display()是ClassA的，因为覆盖了，show()是Base自己的  
+    a.exec();//结果同上  
+    a.show();//show()是ClassA重定义的  
+    base->fun(1);//fun()是Base自己的，因为直接从对象base调用  
+    a.fun(1, 1);//fun()是ClassA重定义的  
+    return 0;  
+}
+>>>>>>> 94720528a2521bfb8e7f1fe1decdaabcd763924d
