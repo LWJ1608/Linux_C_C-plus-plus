074a79a81b5c0e90f1a78042ceac518a23c167dc
LWJ1608 <93906402+LWJ1608@users.noreply.github.com>
2022-04-07 21:22:00 +0800

    C

--- C学习/指针/test.c
+++ C学习/指针/test.c
@@ -1,17 +1,6 @@
 /*
  * @Author: lwj
- * @Date: 2022-04-06 21:05:05
- * @LastEditTime: 2022-04-06 21:18:25
+ * @Date: 2022-04-07 19:53:31
  * @Description:
- * @FilePath: /C初级学习/指针/test.c
  */
 
-#include <stdio.h>
-
-int main()
-{
-    int arr[4] = {1};
-    int(*p)[2] = {&arr};
-    printf("%d", *p);
-    return 0;
-}
