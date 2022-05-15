866685aded4555e69fe36fe3ddda0dfe607d9a6e
LWJ1608 <2194677100@qq.com>
2022-05-15 19:10:33 +0800

    test

--- aotuPush.sh
+++ aotuPush.sh
@@ -11,14 +11,14 @@ git_push(){
     echo "-------切换目录-------"
     echo `pwd`
     echo "---------------------"
-    # if [ $[ ${modify_time}-${this_time} ] -gt 86400 ];
-    # then
+    if [ $[ ${modify_time}-${this_time} ] -gt 0 ];
+    then
         echo "${1} 文件夹 有变化，正在准备push..."
         date=`date "+%Y-%m-%d %H:%M:%S"`
         git add .    
         git commit -m "push @$(date)"
         git push origin master
-    # fi
+    fi
 }
 
 git_push /root/Linux_C_C-plus-plus
