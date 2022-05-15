d64f5448f94e4036e7980522cf3a661d39994964
LWJ1608 <2194677100@qq.com>
2022-05-15 20:03:28 +0800

    push @2022年 05月 15日 星期日 20:03:28 CST

--- autoPush.sh
+++ autoPush.sh
@@ -3,15 +3,11 @@
 path=/root/Linux_C_C-plus-plus/
 
 git_push(){
-    echo "-------开始push------"
-
-
     cd ${1}
-        date=`date "+%Y-%m-%d %H:%M:%S"`
-        git add .    
-        git commit -m "push @$(date)"
-        git push origin master
-
+    date=`date "+%Y-%m-%d %H:%M:%S"`
+    git add .    
+    git commit -m "push @$(date)"
+    git push origin master
 }
 
 git_push /root/Linux_C_C-plus-plus
