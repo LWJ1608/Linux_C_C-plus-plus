54787b204d356f822d805a93b9205a46ea2dd2dc
LWJ1608 <2194677100@qq.com>
2022-05-15 22:17:03 +0800

    push @2022年 05月 15日 星期日 22:17:03 CST

--- autoPush.sh
+++ autoPush.sh
@@ -1,6 +1,22 @@
+# #!/bin/bash
+
+
+# cd /root/Linux_C_C-plus-plus/
+# git add . && git commit -m "push @$(date)" && git push -u origin master
+
+
 #!/bin/bash
-
-
-cd /root/Linux_C_C-plus-plus/
-git add . && git commit -m "push @$(date)" && git push -u origin master
-
+echo "push_git.sh start……"
+push(){
+    cd /root/Linux_C_C-plus-plus/
+    git add .
+    git commit -m "push @$(date)"
+    git push origin master
+}
+project_array=(project_name1 project_name2 project_name3 project_name4 project_name5 project_name6 project_name7 project_name8)
+for data in ${project_array[@]}  
+do  
+    echo "project name *************"+${data}+"*************"
+    push ${data}
+done       
+echo "push_git.sh end……"
