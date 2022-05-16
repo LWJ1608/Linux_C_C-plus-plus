507c3dbbeec6516be070cb1e5d3e1ccc38688f88
LWJ1608 <2194677100@qq.com>
2022-05-16 22:31:03 +0800

    push @2022年 05月 16日 星期一 22:31:03 CST

--- C++提高/STL-函数对象/内建函数对象/test02.cpp
+++ C++提高/STL-函数对象/内建函数对象/test02.cpp
@@ -3,6 +3,6 @@
  * @Date: 2022-05-16 22:30:42
  * @LastEditTime: 2022-05-16 22:30:42
  * @LastEditors: lwj
- * @Description:
+ * @Description:关系仿函数
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-函数对象/内建函数对象/test02.cpp
  **/
