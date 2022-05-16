7ef1effcd6614bed1dfdb48ae0ccfc58c498ca61
LWJ1608 <2194677100@qq.com>
2022-05-16 22:33:01 +0800

    push @2022年 05月 16日 星期一 22:33:01 CST

--- C++提高/STL-函数对象/内建函数对象/test02.cpp
+++ C++提高/STL-函数对象/内建函数对象/test02.cpp
@@ -6,6 +6,13 @@
  * @Description:关系仿函数
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-函数对象/内建函数对象/test02.cpp
  **/
+//函数对象原型：
+// * `template<class T> bool equal_to<T>`                    //等于
+// * `template<class T> bool not_equal_to<T>`            //不等于
+// * `template<class T> bool greater<T>`                      //大于
+// * `template<class T> bool greater_equal<T>`          //大于等于
+// * `template<class T> bool less<T>`                           //小于
+// * `template<class T> bool less_equal<T>`               //小于等于
 #include <iostream>
 #include <functional> //内联函数对象头文件
 
