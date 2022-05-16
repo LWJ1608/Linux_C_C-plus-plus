798d45487f2f7d0ef0cb8f8f3b08dad88c3afefb
LWJ1608 <2194677100@qq.com>
2022-05-16 22:08:01 +0800

    push @2022年 05月 16日 星期一 22:08:01 CST

--- C++提高/STL-函数对象/内建函数对象/test01.cpp
+++ C++提高/STL-函数对象/内建函数对象/test01.cpp
@@ -3,6 +3,18 @@
  * @Date: 2022-05-16 22:06:37
  * @LastEditTime: 2022-05-16 22:06:38
  * @LastEditors: lwj
- * @Description:
+ * @Description:算术仿函数
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-函数对象/内建函数对象/test01.cpp
  **/
+
+#include <iostream>
+#include <functional>
+
+void test01()
+{
+}
+int main()
+{
+
+    return 0;
+}
