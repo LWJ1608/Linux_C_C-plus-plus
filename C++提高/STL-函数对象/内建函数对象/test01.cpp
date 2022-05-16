38ab24c6c7464d585a4cd04a58ebc11276e0021e
LWJ1608 <2194677100@qq.com>
2022-05-16 22:10:01 +0800

    push @2022年 05月 16日 星期一 22:10:01 CST

--- C++提高/STL-函数对象/内建函数对象/test01.cpp
+++ C++提高/STL-函数对象/内建函数对象/test01.cpp
@@ -7,11 +7,11 @@
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-函数对象/内建函数对象/test01.cpp
  **/
 // * `template<class T> T plus<T>`                //加法仿函数
-// * `template<class T> T minus<T>`              //减法仿函数
-// * `template<class T> T multiplies<T>`    //乘法仿函数
-// * `template<class T> T divides<T>`         //除法仿函数
-// * `template<class T> T modulus<T>`         //取模仿函数
-// * `template<class T> T negate<T>`           //取反仿函数
+// * `template<class T> T minus<T>`               //减法仿函数
+// * `template<class T> T multiplies<T>`          //乘法仿函数
+// * `template<class T> T divides<T>`             //除法仿函数
+// * `template<class T> T modulus<T>`             //取模仿函数
+// * `template<class T> T negate<T>`              //取反仿函数
 #include <iostream>
 #include <functional>
 
