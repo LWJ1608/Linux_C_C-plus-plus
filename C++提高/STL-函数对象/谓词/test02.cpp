eae6e91791df2036f61acdae82c9441de1403d69
LWJ1608 <2194677100@qq.com>
2022-05-16 22:03:01 +0800

    push @2022年 05月 16日 星期一 22:03:01 CST

--- C++提高/STL-函数对象/谓词/test02.cpp
+++ C++提高/STL-函数对象/谓词/test02.cpp
@@ -13,22 +13,41 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
-struct CompareNum
+//二元谓词
+class MyCompare
 {
-    bool operator()(int val1, int val2) //二元谓词
+public:
+    bool operator()(int num1, int num2)
     {
-        return val1 > val2;
+        return num1 > num2;
     }
 };
 
 void test01()
 {
-    std::vector<int> v1;
-    for (int i = 5; i < 10; i++)
+    std::vector<int> v;
+    v.push_back(10);
+    v.push_back(40);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(50);
+
+    //默认从小到大
+    sort(v.begin(), v.end());
+    for (std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
     {
-        v1.push_back(i);
+        cout << *it << " ";
     }
+    std::cout << std::endl;
+    cout << "----------------------------" << endl;
+
+    //使用函数对象改变算法策略，排序从大到小
+    sort(v.begin(), v.end(), MyCompare());
+    for (std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
 }
 
 int main()
