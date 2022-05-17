64e01ce276420300ce96e2e6f6f83b21f3086d5b
LWJ1608 <2194677100@qq.com>
2022-05-17 21:42:01 +0800

    push @2022年 05月 17日 星期二 21:42:01 CST

--- C++提高/STL-函数对象/内建函数对象/test0.cpp
+++ C++提高/STL-函数对象/内建函数对象/test0.cpp
@@ -6,6 +6,9 @@
  * @Description:逻辑仿函数
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-函数对象/内建函数对象/test0.cpp
  **/
+// * `template<class T> bool logical_and<T>`              //逻辑与
+// * `template<class T> bool logical_or<T>`                //逻辑或
+// * `template<class T> bool logical_not<T>`              //逻辑非
 
 #include <iostream>
 #include <functional>
