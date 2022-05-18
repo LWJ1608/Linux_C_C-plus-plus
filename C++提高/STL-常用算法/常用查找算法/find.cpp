d89ebe7ee6563fba8bc38016ca0a62e7752b879a
LWJ1608 <2194677100@qq.com>
2022-05-18 21:33:01 +0800

    push @2022年 05月 18日 星期三 21:33:01 CST

--- C++提高/STL-常用算法/常用查找算法/find.cpp
+++ C++提高/STL-常用算法/常用查找算法/find.cpp
@@ -6,6 +6,16 @@
  * @Description:find算法的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用查找算法/find.cpp
  **/
+
+//功能描述：
+// * 查找指定元素，找到返回指定元素的迭代器，找不到返回结束迭代器end()
+
+// **函数原型：**
+// - `find(iterator beg, iterator end, value);  `
+// 按值查找元素，找到返回指定位置迭代器，找不到返回结束迭代器位置
+// beg 开始迭代器
+// end 结束迭代器
+// value 查找的元素
 #include <iostream>
 #include <algorithm>
 
