83da0100567245dc9c62743576b5487d819c5f93
LWJ1608 <2194677100@qq.com>
2022-05-18 21:04:01 +0800

    push @2022年 05月 18日 星期三 21:04:01 CST

--- C++提高/STL-常用算法/常用遍历算法/for_each.cpp
+++ C++提高/STL-常用算法/常用遍历算法/for_each.cpp
@@ -7,30 +7,30 @@
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/for_each.cpp
  **/
 
-//for_each(iterator beg, iterator end, _func);  
-// 遍历算法 遍历容器元素
-// beg 开始迭代器
-// end 结束迭代器
-// _func 函数或者函数对象
-
+// for_each(iterator beg, iterator end, _func);
+//  遍历算法 遍历容器元素
+//  beg 开始迭代器
+//  end 结束迭代器
+//  _func 函数或者函数对象
 
 #include <iostream>
-#include<algorithm>
+#include <vector>
+#include <algorithm>
 
 //普通函数
 void printV(int val)
 {
-    std::cout<<val<<" ";
+    std::cout << val << " ";
 }
 void test01()
 {
     std::vector<int> v1;
-    for(int i=0;i<5;i++)
+    for (int i = 0; i < 5; i++)
     {
         v1.push_back(i);
     }
-    std::for_each(v1.begin(),v1.end(),printV);//传入普通函数名
-    std::cout<<std::endl;
+    std::for_each(v1.begin(), v1.end(), printV); //传入普通函数名
+    std::cout << std::endl;
 }
 int main()
 {
