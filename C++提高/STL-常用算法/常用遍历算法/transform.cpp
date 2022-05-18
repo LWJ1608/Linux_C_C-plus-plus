5db4f62c8693303915170de6b52dc0f925edb8f4
LWJ1608 <2194677100@qq.com>
2022-05-18 21:11:01 +0800

    push @2022年 05月 18日 星期三 21:11:01 CST

--- C++提高/STL-常用算法/常用遍历算法/transform.cpp
+++ C++提高/STL-常用算法/常用遍历算法/transform.cpp
@@ -3,6 +3,11 @@
  * @Date: 2022-05-18 21:09:28
  * @LastEditTime: 2022-05-18 21:09:29
  * @LastEditors: lwj
- * @Description:transform
+ * @Description:transform算法的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用遍历算法/transform.cpp
  **/
+// transform(iterator beg1, iterator end1, iterator beg2, _func);
+`   //beg1 源容器开始迭代器
+    // end1 源容器结束迭代器
+    // beg2 目标容器开始迭代器
+    //_func 函数或者函数对象
