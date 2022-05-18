076c816395af8cac39c6ffcc62c79e75dab05635
LWJ1608 <2194677100@qq.com>
2022-05-18 20:44:01 +0800

    push @2022年 05月 18日 星期三 20:44:01 CST

--- C++提高/STL-常用算法/for_each.cpp
+++ C++提高/STL-常用算法/for_each.cpp
@@ -3,6 +3,6 @@
  * @Date: 2022-05-18 20:43:00
  * @LastEditTime: 2022-05-18 20:43:01
  * @LastEditors: lwj
- * @Description:
+ * @Description:for_each算法的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/for_each.cpp
  **/
