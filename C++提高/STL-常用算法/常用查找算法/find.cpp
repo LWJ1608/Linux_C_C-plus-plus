d9cfa5232bf79837cf2b4e34a9c8b5d81e9d91de
LWJ1608 <2194677100@qq.com>
2022-05-18 21:32:01 +0800

    push @2022年 05月 18日 星期三 21:32:01 CST

--- C++提高/STL-常用算法/常用查找算法/find.cpp
+++ C++提高/STL-常用算法/常用查找算法/find.cpp
@@ -3,6 +3,17 @@
  * @Date: 2022-05-18 21:29:16
  * @LastEditTime: 2022-05-18 21:29:16
  * @LastEditors: lwj
- * @Description:
+ * @Description:find算法的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用查找算法/find.cpp
  **/
+#include <iostream>
+#include <algorithm>
+
+void test01()
+{
+}
+int main()
+{
+    test01();
+    return 0;
+}
