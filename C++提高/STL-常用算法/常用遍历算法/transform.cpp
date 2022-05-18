e771e5b823d45e3e48117e84edafc0b88dfd0f2e
LWJ1608 <2194677100@qq.com>
2022-05-18 21:26:01 +0800

    push @2022年 05月 18日 星期三 21:26:01 CST

--- C++提高/STL-常用算法/常用遍历算法/transform.cpp
+++ C++提高/STL-常用算法/常用遍历算法/transform.cpp
@@ -7,7 +7,7 @@
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用遍历算法/transform.cpp
  **/
 
-// transform(iterator beg1, iterator end1, iterator beg2, _func);
+// transform(iterator beg1, iterator end1, iterator beg2, _func);搬运容器到另一个容器中
 // beg1 源容器开始迭代器
 // end1 源容器结束迭代器
 // beg2 目标容器开始迭代器
