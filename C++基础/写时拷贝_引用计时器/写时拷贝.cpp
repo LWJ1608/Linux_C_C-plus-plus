4660ab0eb169e38c80401fceb079412dbd6d86e9
LWJ1608 <2194677100@qq.com>
2022-05-19 11:21:01 +0800

    push @2022年 05月 19日 星期四 11:21:01 CST

--- C++基础/写时拷贝_引用计时器/写时拷贝.cpp
+++ C++基础/写时拷贝_引用计时器/写时拷贝.cpp
@@ -1,8 +1,8 @@
 /**
-*@author lwj
-*修改日期 2022.3.18
-*写时拷贝简单实现
-**/
+ *@author lwj
+ *修改日期 2022.3.18
+ *写时拷贝简单实现
+ **/
 
 //写时拷贝：
 //目的是保证数据不发生泄漏的同时，合理利用内存空间，
@@ -12,45 +12,45 @@
 
 //
 
-
-#include<iostream>
-#include<string>
-#include<malloc.h>
+#include <iostream>
+#include <string.h>
+#include <malloc.h>
 using namespace std;
 
 class String;
-class String_rep//这个类主要是进行数据存储的
+class String_rep //这个类主要是进行数据存储的
 {
-friend class String;//友元类
+	friend class String; //友元类
 public:
-	String_rep(const char* str=""):use_count(0)
+	String_rep(const char *str = "") : use_count(0)
 	{
-		if(!str)
+		if (!str)
 		{
-			data=new char(strlen(str)+1);
-			strcpy(data,str);
+			data = new char(strlen(str) + 1);
+			strcpy(data, str);
 		}
 		else
 		{
-			data=new char[1];
-			data[0]=0;
+			data = new char[1];
+			data[0] = 0;
 		}
 	}
 	String_rep(const String_rep &s);
-	String_rep& operator=(const String_rep &s); 
+	String_rep &operator=(const String_rep &s);
 	~String()
-		{
-			delete []data;
-			data=NULL;
-		}
+	{
+		delete[] data;
+		data = NULL;
+	}
+
 public:
-	void increment()//指向对象空间时+1
+	void increment() //指向对象空间时+1
 	{
 		++use_count;
 	}
-	void decrement()//当指向对象空间指针减少时
+	void decrement() //当指向对象空间指针减少时
 	{
-		if(--use_count==0)
+		if (--use_count == 0)
 		{
 			delete this;
 		}
@@ -59,32 +59,33 @@ public:
 	{
 		return use_count;
 	}
-	char* getData()
+	char *getData()
 	{
 		return data;
-	} 
+	}
+
 private:
-	char * data;
+	char *data;
 	int use_count;
 };
-class String//这个类主要是进行数据操作
+class String //这个类主要是进行数据操作
 {
 public:
-	String(const char* str=""):rep(new String_rep(str))
+	String(const char *str = "") : rep(new String_rep(str))
 	{
 		rep->increment();
 	}
-	String(const String &s)//拷贝构造
-	{	
-		rep=s.rep;
+	String(const String &s) //拷贝构造
+	{
+		rep = s.rep;
 		rep->increment();
 	}
-	String& operator=(const String &s)//赋值重载
+	String &operator=(const String &s) //赋值重载
 	{
-		if(rep!=s)
+		if (rep != s)
 		{
 			rep->decrement();
-			rep=s.rep;
+			rep = s.rep;
 			rep->decrement();
 		}
 	}
@@ -92,47 +93,44 @@ public:
 	{
 		rep->decrement();
 	}
+
 public:
-	int  print_use_count1()
+	int print_use_count1()
 	{
 		return rep->print_use_count();
 	}
-	char* pirnt_data()
+	char *pirnt_data()
 	{
 		return rep->getData();
 	}
 
 	void toupper()
 	{
-		if(rep->print_use_count()>1)//当对象个数大于1时
+		if (rep->print_use_count() > 1) //当对象个数大于1时
 		{
 			rep->decrement();
 			String_rep *rep_new = new String_rep(rep->data);
-			rep=rep_new;
+			rep = rep_new;
 			rep->increment();
 		}
 	}
+
 private:
-String_rep* rep;
+	String_rep *rep;
 };
 
 int main()
 {
-    String s1("hello");
-    String s2 = s1;
-    String s3("xyz");
-    s3 = s2;
-    s1.toupper();
-    s1.print_data();
-    cout<<"s1 count = "<<s1.print_use_count1()<<endl;
-    s2.print_data();
-    cout<<"s2 count = "<<s2.print_use_count1()<<endl;
-    s3.print_data();
-    cout<<"s3 count = "<<s3.print_use_count1()<<endl;
-    return 0;
+	String s1("hello");
+	String s2 = s1;
+	String s3("xyz");
+	s3 = s2;
+	s1.toupper();
+	s1.print_data();
+	cout << "s1 count = " << s1.print_use_count1() << endl;
+	s2.print_data();
+	cout << "s2 count = " << s2.print_use_count1() << endl;
+	s3.print_data();
+	cout << "s3 count = " << s3.print_use_count1() << endl;
+	return 0;
 }
-
-
-
-
-
