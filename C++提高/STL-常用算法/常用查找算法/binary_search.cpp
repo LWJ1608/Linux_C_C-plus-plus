d16b8cbaf53104a734ea4a75b8561c6ac48d98fc
LWJ1608 <2194677100@qq.com>
2022-05-19 13:41:01 +0800

    push @2022年 05月 19日 星期四 13:41:01 CST

--- C++提高/STL-常用算法/常用查找算法/binary_search.cpp
+++ C++提高/STL-常用算法/常用查找算法/binary_search.cpp
@@ -6,3 +6,12 @@
  * @Description:binary_search（二分查找法）的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用查找算法/binary_search.cpp
  **/
+// **功能描述：**
+// * 查找指定元素是否存在
+// **函数原型：**
+// - `bool binary_search(iterator beg, iterator end, value);
+// 查找指定的元素，查到 返回true  否则false
+// 注意: 在**无序序列中不可用**
+// beg 开始迭代器
+// end 结束迭代器
+// value 查找的元素
