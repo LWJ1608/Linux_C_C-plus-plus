bb1ab99c83b0433ff74615206560a057f3e780c2
LWJ1608 <2194677100@qq.com>
2022-05-19 20:20:01 +0800

    push @2022年 05月 19日 星期四 20:20:01 CST

--- C++提高/STL-常用算法/常用查找算法/count.cpp
+++ C++提高/STL-常用算法/常用查找算法/count.cpp
@@ -16,7 +16,7 @@
 // value 统计的元素
 #include <iostream>
 #include <queue>
-#include<algorithm>
+#include <algorithm>
 
 void printQueue(std::queue<int> &v)
 {
@@ -31,7 +31,7 @@ void test01()
     std::queue<int> q1;
     for (int i = 0; i < 8; i++)
     {
-        q1.push(i+1);
+        q1.
     }
     printQueue(q1);
 }
