c656103ca8e96094556f923c03697a928b256a78
LWJ1608 <2194677100@qq.com>
2022-05-19 20:17:01 +0800

    push @2022年 05月 19日 星期四 20:17:01 CST

--- C++提高/STL-常用算法/常用查找算法/count.cpp
+++ C++提高/STL-常用算法/常用查找算法/count.cpp
@@ -18,6 +18,14 @@
 #include <queue>
 #include<algorithm>
 
+void printQueue(std::queue<int> &v)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        std::cout << v.back() << " ";
+    }
+    std::cout << std::endl;
+}
 void test01()
 {
     std::queue<int> q1;
@@ -25,7 +33,7 @@ void test01()
     {
         q1.push(i);
     }
-    
+    printQueue(q1);
 }
 int main()
 {
