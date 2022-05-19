d63883e6a517d054f248ec3a6f1823cd947af41a
LWJ1608 <2194677100@qq.com>
2022-05-19 11:42:01 +0800

    push @2022年 05月 19日 星期四 11:42:01 CST

--- C++提高/STL-常用算法/常用查找算法/find_if.cpp
+++ C++提高/STL-常用算法/常用查找算法/find_if.cpp
@@ -10,7 +10,7 @@
 // * 按条件查找元素
 //函数原型：
 // find_if(iterator beg, iterator end, _Pred);
-// 按值查找元素，找到返回指定位置迭代器，找不到返回结束迭代器位置
+// 按条件查找元素，找到返回指定位置迭代器，找不到返回结束迭代器位置
 // beg 开始迭代器
 // end 结束迭代器
 // _Pred 函数或者谓词（返回bool类型的仿函数）
@@ -19,7 +19,7 @@
 #include <string>
 #include <algorithm>
 
-//系统数据类型查找
+//数据类型查找
 void test01()
 {
 }
