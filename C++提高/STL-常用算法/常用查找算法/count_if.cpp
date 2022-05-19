3139935cc573a34c5dff35a504adfb811bbe1368
LWJ1608 <2194677100@qq.com>
2022-05-19 20:33:01 +0800

    push @2022年 05月 19日 星期四 20:33:01 CST

--- C++提高/STL-常用算法/常用查找算法/count_if.cpp
+++ C++提高/STL-常用算法/常用查找算法/count_if.cpp
@@ -3,6 +3,14 @@
  * @Date: 2022-05-19 20:31:25
  * @LastEditTime: 2022-05-19 20:31:26
  * @LastEditors: lwj
- * @Description:
+ * @Description:count_if算法的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用查找算法/count_if.cpp
  **/
+// **功能描述：**
+// * 按条件统计元素个数
+// **函数原型：**
+// - `count_if(iterator beg, iterator end, _Pred);  `
+// 按条件统计元素出现次数
+// beg 开始迭代器
+// end 结束迭代器
+// _Pred 谓词
