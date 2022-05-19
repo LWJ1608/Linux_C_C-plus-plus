f3a2fee590f3ec7078eb9148465692f384eaa94d
LWJ1608 <2194677100@qq.com>
2022-05-19 13:27:01 +0800

    push @2022年 05月 19日 星期四 13:27:01 CST

--- C++提高/STL-常用算法/常用查找算法/adjacent_find.cpp
+++ C++提高/STL-常用算法/常用查找算法/adjacent_find.cpp
@@ -3,6 +3,21 @@
  * @Date: 2022-05-19 13:24:44
  * @LastEditTime: 2022-05-19 13:24:45
  * @LastEditors: lwj
- * @Description:
+ * @Description:adjacent_find查找算法的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用查找算法/adejacent.cpp
  **/
+// **功能描述：**
+// * 查找相邻重复元素
+// **函数原型：**
+// - `adjacent_find(iterator beg, iterator end);  `
+// 查找相邻重复元素,返回相邻元素的第一个位置的迭代器
+// beg 开始迭代器
+// end 结束迭代器
+#include <iostream>
+#include <vector>
+
+int main()
+{
+
+    return 0;
+}
