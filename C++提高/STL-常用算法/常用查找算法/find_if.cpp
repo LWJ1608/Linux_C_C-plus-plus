59d496917f322ce05e4d5975745062227f27fbeb
LWJ1608 <2194677100@qq.com>
2022-05-19 11:50:01 +0800

    push @2022年 05月 19日 星期四 11:50:01 CST

--- C++提高/STL-常用算法/常用查找算法/find_if.cpp
+++ C++提高/STL-常用算法/常用查找算法/find_if.cpp
@@ -31,6 +31,18 @@ public:
 //内置数据类型查找
 void test01()
 {
+    std::vector<Person> v1;
+    Person p1("小文", 33);
+    Person p2("小五", 13);
+    Person p3("小搜", 19);
+    Person p4("小数", 12);
+    Person p5("小串", 39);
+
+    Person1.push_back(p1);
+    Person1.push_back(p2);
+    Person1.push_back(p3);
+    Person1.push_back(p4);
+    Person1.push_back(p5);
 }
 int main()
 {
