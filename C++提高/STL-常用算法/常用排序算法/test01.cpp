79887cb55018ac5a210c426bad9fd3f8f3e08472
LWJ1608 <2194677100@qq.com>
2022-05-20 20:32:01 +0800

    push @2022年 05月 20日 星期五 20:32:01 CST

--- C++提高/STL-常用算法/常用排序算法/test01.cpp
+++ C++提高/STL-常用算法/常用排序算法/test01.cpp
@@ -6,6 +6,14 @@
  * @Description:
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用排序算法/test01.cpp
  **/
+// **功能描述：**
+// * 对容器内元素进行排序
+// **函数原型：**
+// - `sort(iterator beg, iterator end, _Pred);  `
+// 按值查找元素，找到返回指定位置迭代器，找不到返回结束迭代器位置
+//  beg    开始迭代器
+//  end    结束迭代器
+// _Pred  谓词
 
 #include <iostream>
 #include <vector>
