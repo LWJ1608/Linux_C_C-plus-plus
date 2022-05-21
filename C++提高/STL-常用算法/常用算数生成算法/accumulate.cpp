fcbd3d6b2bb8fcf0302048a62128cf2a415e8895
LWJ1608 <2194677100@qq.com>
2022-05-21 22:50:01 +0800

    push @2022年 05月 21日 星期六 22:50:01 CST

---
+++ C++提高/STL-常用算法/常用算数生成算法/accumulate.cpp
@@ -0,0 +1,8 @@
+/**
+ * @Author: lwj
+ * @Date: 2022-05-21 22:49:56
+ * @LastEditTime: 2022-05-21 22:49:56
+ * @LastEditors: lwj
+ * @Description:
+ * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用算数生成算法/accumulate.cpp
+ **/
