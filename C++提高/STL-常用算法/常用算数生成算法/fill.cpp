664f88f0c8ae4873b343c0cd5e1b14224bdf2f93
LWJ1608 <2194677100@qq.com>
2022-05-21 22:58:01 +0800

    push @2022年 05月 21日 星期六 22:58:01 CST

--- C++提高/STL-常用算法/常用算数生成算法/fill.cpp
+++ C++提高/STL-常用算法/常用算数生成算法/fill.cpp
@@ -6,3 +6,35 @@
  * @Description:fill算法的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用算数生成算法/fill.cpp
  **/
+#include<iostream>
+#include <numeric>
+#include <vector>
+#include <algorithm>
+
+class myPrint
+{
+public:
+    void operator()(int val)
+    {
+        std::cout << val << " ";
+    }
+};
+
+void test01()
+{
+
+    std::vector<int> v;
+    v.resize(10);
+    //填充
+    std::fill(v.begin(), v.end(), 100);
+
+    std::for_each(v.begin(), v.end(), myPrint());
+    std::cout << std::endl;
+}
+
+int main()
+{
+
+    test01();
+    return 0;
+}
