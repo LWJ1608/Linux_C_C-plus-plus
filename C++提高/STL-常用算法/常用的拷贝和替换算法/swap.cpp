bdfae189958540974ce27116271a89423fa4f56c
LWJ1608 <2194677100@qq.com>
2022-05-21 22:32:01 +0800

    push @2022年 05月 21日 星期六 22:32:01 CST

--- C++提高/STL-常用算法/常用的拷贝和替换算法/swap.cpp
+++ C++提高/STL-常用算法/常用的拷贝和替换算法/swap.cpp
@@ -3,6 +3,13 @@
  * @Date: 2022-05-21 22:30:02
  * @LastEditTime: 2022-05-21 22:30:02
  * @LastEditors: lwj
- * @Description:
+ * @Description:swap算法的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用的拷贝和替换算法/swap.cpp
  **/
+// **功能描述：**
+// * 互换两个容器的元素
+// **函数原型：**
+// - `swap(container c1, container c2);  `
+// 互换两个容器的元素
+// c1容器1
+// c2容器2
