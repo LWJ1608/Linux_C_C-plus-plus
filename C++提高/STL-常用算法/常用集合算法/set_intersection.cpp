498e7fe06aa1c7f58465d742b8607b770687d911
LWJ1608 <2194677100@qq.com>
2022-05-21 23:10:01 +0800

    push @2022年 05月 21日 星期六 23:10:01 CST

--- C++提高/STL-常用算法/常用集合算法/set_intersection.cpp
+++ C++提高/STL-常用算法/常用集合算法/set_intersection.cpp
@@ -6,3 +6,23 @@
  * @Description:set_intersection算法的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用集合算法/set_intersection.cpp
  **/
+// **功能描述：**
+// * 求两个容器的交集
+// **函数原型：**
+// - `set_intersection(iterator beg1, iterator end1, iterator beg2, iterator end2, iterator dest);  `
+// 求两个集合的交集
+// **注意:两个集合必须是有序序列**
+// beg1 容器1开始迭代器
+// end1 容器1结束迭代器
+// beg2 容器2开始迭代器
+// end2 容器2结束迭代器
+// dest 目标容器开始迭代器
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+
+    return 0;
+}
