e53c326ef6a7d97494412f7bdabb2c8793cda27b
LWJ1608 <2194677100@qq.com>
2022-05-21 19:48:01 +0800

    push @2022年 05月 21日 星期六 19:48:01 CST

---
+++ C++提高/STL-常用算法/常用排序算法/random_shuffle.cpp
@@ -0,0 +1,29 @@
+/**
+ * @Author: lwj
+ * @Date: 2022-05-21 19:43:31
+ * @LastEditTime: 2022-05-21 19:43:32
+ * @LastEditors: lwj
+ * @Description:random_shuffle随机排序算法的使用
+ * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用排序算法/random_shuffe.cpp
+ **/
+
+// **功能描述：**
+// * 洗牌   指定范围内的元素随机调整次序
+// **函数原型：**
+// - `random_shuffle(iterator beg, iterator end);  `
+// 指定范围内的元素随机调整次序
+// beg 开始迭代器
+// end 结束迭代器
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void test01()
+{
+    
+}
+int main()
+{
+
+    return 0;
+}
