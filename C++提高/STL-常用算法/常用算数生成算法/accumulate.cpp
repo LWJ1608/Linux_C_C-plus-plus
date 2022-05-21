b0aabe73dfa75455f54c3b3a93c194964a114f0c
LWJ1608 <2194677100@qq.com>
2022-05-21 22:51:02 +0800

    push @2022年 05月 21日 星期六 22:51:02 CST

--- C++提高/STL-常用算法/常用算数生成算法/accumulate.cpp
+++ C++提高/STL-常用算法/常用算数生成算法/accumulate.cpp
@@ -3,6 +3,14 @@
  * @Date: 2022-05-21 22:49:56
  * @LastEditTime: 2022-05-21 22:49:56
  * @LastEditors: lwj
- * @Description:
+ * @Description:accumulate算法的使用
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用算数生成算法/accumulate.cpp
  **/
+// **功能描述：**
+// *  计算区间内 容器元素累计总和
+// **函数原型：**
+// - `accumulate(iterator beg, iterator end, value);  `
+// 计算容器元素累计总和
+// beg 开始迭代器
+// end 结束迭代器
+// value 起始值
