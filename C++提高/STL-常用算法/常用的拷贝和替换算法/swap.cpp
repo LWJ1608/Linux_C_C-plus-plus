5c4a4fd6c2338b60b9ad05e76ea744edd4b0b0d8
LWJ1608 <2194677100@qq.com>
2022-05-21 22:38:01 +0800

    push @2022年 05月 21日 星期六 22:38:01 CST

--- C++提高/STL-常用算法/常用的拷贝和替换算法/swap.cpp
+++ C++提高/STL-常用算法/常用的拷贝和替换算法/swap.cpp
@@ -13,16 +13,33 @@
 // 互换两个容器的元素
 // c1容器1
 // c2容器2
-#include<iostream>
-#include<vector>
-#include<algorithm>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
+//谓词，定义输出格式
+class myPrint
+{
+public:
+    void operator()(int val)
+    {
+        std::cout << val << " ";
+    }
+};
 void test01()
 {
-    
+    std::vector<int> v1;
+    std::vector<int> v2;
+    for (int i = 0; i < 8; i++)
+    {
+        v1.push_back(i);
+        v2.push_back(i + 8);
+    }
+    std::for_each(v1.begin(), v1.end(), myPrint());
+    std::for_each(v2.begin(), v2.end(), myPrint());
+    // std::swap(v1.begin(),v1.end(),v2.begin(),v2.end());
 }
 int main()
 {
-
+    test01();
     return 0;
 }
