abb2970ea684913d8158e5c6c88f181a71020275
LWJ1608 <2194677100@qq.com>
2022-05-21 22:15:01 +0800

    push @2022年 05月 21日 星期六 22:15:01 CST

--- C++提高/STL-常用算法/常用的拷贝和替换算法/replace_if.cpp
+++ C++提高/STL-常用算法/常用的拷贝和替换算法/replace_if.cpp
@@ -16,12 +16,32 @@
 // _pred 谓词
 // newvalue 替换的新元素
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+
+class myPrint
+{
+public:
+    void operator()(int val)
+    {
+        std::cout << val << " ";
+    }
+};
 
 void test01()
 {
-    
+    std::vector<int> v;
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(20);
+    v.push_back(40);
+    v.push_back(50);
+    v.push_back(10);
+    v.push_back(20);
+
+    std::cout << "替换前：" << std::endl;
+    for_each(v.begin(), v.end(), myPrint());
+    std::cout << std::endl;
 }
 int main()
 {
