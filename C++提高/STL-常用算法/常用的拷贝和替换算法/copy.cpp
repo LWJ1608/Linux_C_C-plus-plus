71e9f8983d15cc8e9b785a0cf293f43408c82451
LWJ1608 <2194677100@qq.com>
2022-05-21 21:49:03 +0800

    push @2022年 05月 21日 星期六 21:49:03 CST

--- C++提高/STL-常用算法/常用的拷贝和替换算法/copy.cpp
+++ C++提高/STL-常用算法/常用的拷贝和替换算法/copy.cpp
@@ -6,3 +6,8 @@
  * @Description:
  * @FilePath: /Linux_C_C-plus-plus/C++提高/STL-常用算法/常用的拷贝和替换算法/copy.cpp
  **/
+// **功能描述：**
+// * 容器内指定范围的元素拷贝到另一容器中
+// **函数原型：**
+// - `copy(iterator beg, iterator end, iterator dest);  `
+// 按值查找元素，找到返回指定位置迭代器，找不到返回结束迭代器位置
