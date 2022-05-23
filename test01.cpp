e6dc54ab96ee3d4521ff1875ac9e060ea280a2ef
LWJ1608 <2194677100@qq.com>
2022-05-23 13:14:01 +0800

    push @2022年 05月 23日 星期一 13:14:01 CST

--- test01.cpp
+++ test01.cpp
@@ -14,8 +14,8 @@ int main()
     int a = 10;
     //引用的定义
 
-    int &b = a; //
-    // int &b = 10;错误，定义时
+    int &b = a; //可以理解为
+    // int &b = 10;错误，定义时不能等于实参
     //  int &c;错误，引用定义时需要给它赋值
 
     std::cout << "a = " << a << std::endl;
