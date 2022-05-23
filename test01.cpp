868aeaef321a150136afb9964ae3ceb7a812e486
LWJ1608 <2194677100@qq.com>
2022-05-23 16:17:01 +0800

    push @2022年 05月 23日 星期一 16:17:01 CST

--- test01.cpp
+++ test01.cpp
@@ -6,14 +6,36 @@
  * @Description:
  * @FilePath: /Linux_C_C-plus-plus/test01.cpp
  **/
-#include <iostream>
-
-int main()
+#include <stdio.h>
+#include <string.h>
+void sortByA(char **str, int count);
+void sortByA(char **str, int count)
 {
-
-    int a = 10;
-    int c = 9;
-    const int *b = &a;
-    *b = 9;
-    return 0;
+    int i;
+    int j;
+    char *tmp;
+    for (i = 0; i < count; i++)
+    {
+        for (j = i + 1; j < count; j++)
+        {
+            if ((strcmp(*(str + i), *(str + j))) > 0)
+            { //交换的是指针变量;
+                tmp = *(str + i);
+                *(str + i) = *(str + j);
+                *(str + j) = tmp;
+            }
+        }
+    }
+}
+int main(void)
+{
+    char *array[] = {"aaaa", "bbbb", "ccccc", "dddd"};
+    int count = sizeof(array) / sizeof(array[0]);
+    int i;
+    sortByA(array, count);
+    for (i = 0; i < count; i++)
+    {
+        printf("%s ", array[i]);
+    }
+    printf("\n");
 }
