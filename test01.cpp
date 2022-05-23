4ca7d1958fadb73efdd0c2b175e65bf1b7ed4691
LWJ1608 <2194677100@qq.com>
2022-05-23 13:12:01 +0800

    push @2022年 05月 23日 星期一 13:12:01 CST

--- test01.cpp
+++ test01.cpp
@@ -13,8 +13,10 @@ int main()
 
     int a = 10;
     //引用的定义
-    int &b = a;
-    // int &c;错误，引用定义时需要给它赋值
+
+    int &b = a; //
+    // int &b = 10;错误，定义时
+    //  int &c;错误，引用定义时需要给它赋值
 
     std::cout << "a = " << a << std::endl;
     std::cout << "&b = " << &b << std::endl;
