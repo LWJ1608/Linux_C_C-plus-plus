422a99a495efc95b409505db986264e60124f8be
LWJ1608 <2194677100@qq.com>
2022-05-23 15:57:02 +0800

    push @2022年 05月 23日 星期一 15:57:02 CST

--- test01.cpp
+++ test01.cpp
@@ -12,21 +12,8 @@ int main()
 {
 
     int a = 10;
-    //引用的定义
-
-    //下面语句可以理解为把b的地址取出，换成a的地址
-    int &b = a; 
-    // int &b = 10;错误，定义时不能等于实参
-    //  int &c;错误，引用定义时需要给它赋值
-
-    std::cout << "a = " << a << std::endl;
-    std::cout << "&b = " << &b << std::endl;
-    //输出结果第一个输出a的值，第二个输出b的地址，也是a的地址，用16进制表示
-
-    // b = 100;
-
-    // std::cout << "a = " << a << std::endl;
-    // std::cout << "b = " << b << std::endl;
-
-    return 0;
+    int c = 9;
+    const int *b = &a;
+    b =
+        return 0;
 }
