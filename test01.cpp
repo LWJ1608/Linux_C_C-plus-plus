f433b0bcf7a0f6d24f225b36ed856fb0761e219c
LWJ1608 <2194677100@qq.com>
2022-05-23 13:09:01 +0800

    push @2022年 05月 23日 星期一 13:09:01 CST

---
+++ test01.cpp
@@ -0,0 +1,28 @@
+/**
+ * @Author: lwj
+ * @Date: 2022-05-23 13:08:35
+ * @LastEditTime: 2022-05-23 13:08:35
+ * @LastEditors: lwj
+ * @Description:
+ * @FilePath: /Linux_C_C-plus-plus/test01.cpp
+ **/
+#include <iostream>
+
+int main()
+{
+
+    int a = 10;
+    //引用的定义
+    int &b = a;
+    // int &c;错误，引用定义时需要给它赋值
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "&b = " << &b << std::endl;
+
+    b = 100;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+
+    return 0;
+}
