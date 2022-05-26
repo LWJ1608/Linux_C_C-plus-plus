8916725ed982bfc318103ef2c2f9be371655da37
LWJ1608 <2194677100@qq.com>
2022-05-26 10:35:03 +0800

    push @2022年 05月 26日 星期四 10:35:03 CST

--- 数据结构（C++）/Stack/stack.cpp
+++ 数据结构（C++）/Stack/stack.cpp
@@ -2,6 +2,6 @@
  * @Author: lwj
  * @Date: 2022-05-25 23:33:43
  * @LastEditTime: 2022-05-25 23:33:44
- * @Description:栈
+ * @Description:数据结构---栈
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/stack.cpp
  **/
