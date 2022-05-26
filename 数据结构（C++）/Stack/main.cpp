550ebaadce247d45847af572f61fd89bb7c37a26
LWJ1608 <2194677100@qq.com>
2022-05-26 20:50:01 +0800

    push @2022年 05月 26日 星期四 20:50:01 CST

--- 数据结构（C++）/Stack/main.cpp
+++ 数据结构（C++）/Stack/main.cpp
@@ -12,11 +12,11 @@ using namespace std;
 int main(void)
 {
     Stack<int> s1(8); //指定s1的容量为8
-    for (int i = 0; i < s1.size(); i++)
+    for (int i = 0; i < 8; i++)
     {
         s1.push(i); //插入数据
     }
     Stack<int> s2(s1);
-    cout<<"s1的元素的个数："<<s1.size()<<endl;
+    cout << "s1的元素的个数：" << s1.size() << endl;
     return 0;
 }
