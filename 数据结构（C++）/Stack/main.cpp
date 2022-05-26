ee0bb3c27056e0962fa111c38c84a2180af97102
LWJ1608 <2194677100@qq.com>
2022-05-26 16:03:01 +0800

    push @2022年 05月 26日 星期四 16:03:01 CST

--- 数据结构（C++）/Stack/main.cpp
+++ 数据结构（C++）/Stack/main.cpp
@@ -12,13 +12,6 @@ using namespace std;
 int main(void)
 {
     Stack<char> s(20);
-    char t;
-    s.push(65);
-    s.push(66);
-    s.push(67);
-    s.pop(&t);
-    cout << t << endl;
-    s.lookAllStack();
 
     return 0;
 }
