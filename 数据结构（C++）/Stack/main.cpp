a29f3ebd931b3a4a38f3150c52254b3af71f24b3
LWJ1608 <2194677100@qq.com>
2022-05-26 10:48:03 +0800

    push @2022年 05月 26日 星期四 10:48:03 CST

--- 数据结构（C++）/Stack/main.cpp
+++ 数据结构（C++）/Stack/main.cpp
@@ -5,3 +5,20 @@
  * @Description:测试栈功能
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/Stack/main.cpp
  **/
+#include <iostream>
+#include "stack.h"
+using namespace std;
+
+int main(void)
+{
+    Stack<char> s(20);
+    char t;
+    s.push(65);
+    s.push(66);
+    s.push(67);
+    s.pop(&t);
+    cout << t << endl;
+    s.lookAllStack();
+
+    return 0;
+}
