a2ab1cbad778b440cb5d3ffe3a5b3dcc00af09cf
LWJ1608 <2194677100@qq.com>
2022-05-27 20:41:01 +0800

    push @2022年 05月 27日 星期五 20:41:01 CST

--- 数据结构（C++）/Stack/listStack/main.cpp
+++ 数据结构（C++）/Stack/listStack/main.cpp
@@ -19,6 +19,7 @@ int main(void)
     std::cout << "栈顶元素为：" << L1.getTop() << std::endl;
     std::cout << "移除栈顶元素后：" << L1.pop() << std::endl;
     std::cout << "栈顶元素为：" << L1.getTop() << std::endl;
-
+    std::cout << "栈清空栈元素后：" << L1.clear() << std::endl;
+    std::cout << "栈元素个数为：" << L1.size() << std::endl;
     return 0;
 }
