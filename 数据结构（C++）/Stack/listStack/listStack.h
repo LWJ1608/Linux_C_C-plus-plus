f8e63f84346fb3a5d41d7b4018c8dfd93dbe5403
LWJ1608 <2194677100@qq.com>
2022-05-27 20:46:01 +0800

    push @2022年 05月 27日 星期五 20:46:01 CST

--- 数据结构（C++）/Stack/listStack/listStack.h
+++ 数据结构（C++）/Stack/listStack/listStack.h
@@ -89,7 +89,8 @@ Type ListStack<Type>::getTop() const
 template <typename Type>
 Type ListStack<Type>::pop()
 {
-    assert(!isEmpty()); //断言，如果栈为空程序会报错，程序终止
+    // assert(!isEmpty()); //断言，如果栈为空程序会报错，程序终止
+    perror("pop");
     Node *tmp = nullptr;
     tmp = top;
     top = top->next;
