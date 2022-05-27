9c904974c614897443d46b052396be54b5096996
LWJ1608 <2194677100@qq.com>
2022-05-27 15:56:01 +0800

    push @2022年 05月 27日 星期五 15:56:01 CST

--- 数据结构（C++）/Stack/listStack/listStack.h
+++ 数据结构（C++）/Stack/listStack/listStack.h
@@ -5,77 +5,97 @@
  * @Description:
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/Stack/listStack/listStack.h
  **/
-#pragma once
+// #pragma once
 
+// template <typename Type>
+// class LinkStack
+// {
+// private:
+//     struct Node //把每个数据封装成一个结构体，里面包含data和next
+//     {
+//     public:
+//         Type data;
+//         Node *next;
+//         Node(const Type &value, Node *p = NULL)
+//         {
+//             data = value;
+//             next = p;
+//         }
+//         Node() { next = NULL; }
+//     };
+//     Node *top; //栈顶指针
+// public:
+//     LinkStack() { top = NULL; } //构造函数
+//     ~LinkStack() { clear(); }   //析构函数
+
+// public:
+//     void clear();                                //清空
+//     void push(const Type &value);                //进栈
+//     int size() const;                            //返回链栈大小
+//     bool empty() const { return top == NULL; }   //判空
+//     Type pop();                                  //出栈
+//     Type getTypeop() const { return top->data; } //返回栈顶元素
+// };
+
+// template <typename Type>
+// void LinkStack<Type>::push(const Type &value)
+// {
+//     Node *p;
+//     p = new Node(value, top);
+//     top = p;
+// }
+
+// template <typename Type>
+// void LinkStack<Type>::clear()
+// {
+//     Node *p;
+
+//     while (top)
+//     {
+//         p = top;
+//         top = top->next;
+//         delete p;
+//     }
+// }
+
+// template <typename Type>
+// int LinkStack<Type>::size() const
+// {
+//     int curlength = 0;
+//     Node *p = top;
+//     while (p)
+//     {
+//         ++curlength;
+//         p = p->next;
+//     }
+//     return curlength;
+// }
+
+// template <typename Type>
+// Type LinkStack<Type>::pop()
+// {
+//     Node *p;
+//     p = top;
+//     top = top->next;
+//     return p->data;
+// }
+#include <iostream>
+#include <assert.h>
 template <typename Type>
-class LinkStack
+class ListStack
 {
 private:
-    struct Node //把每个数据封装成一个结构体，里面包含data和next
+    struct Node //将每个结点封装成一个结构体，结构体里包含data和next
     {
     public:
-        Type data;
-        Node *next;
-        Node(const Type &value, Node *p = NULL)
+        Type data;                 //所要存储的数据
+        Node *next;                //指向下个结点的指针
+        Node() { next = nullptr; } //创建出的对象没初始化，则默认它next成员指向空指针
+        Node(const Type &value, Node *p = nullptr)
         {
             data = value;
             next = p;
         }
-        Node() { next = NULL; }
     };
-    Node *top; //栈顶指针
-public:
-    LinkStack() { top = NULL; } //构造函数
-    ~LinkStack() { clear(); }   //析构函数
-
-public:
-    void clear();                                //清空
-    void push(const Type &value);                //进栈
-    int size() const;                            //返回链栈大小
-    bool empty() const { return top == NULL; }   //判空
-    Type pop();                                  //出栈
-    Type getTypeop() const { return top->data; } //返回栈顶元素
+    Node *top; //栈顶指针，指向栈顶
 };
-
-template <typename Type>
-void LinkStack<Type>::push(const Type &value)
-{
-    Node *p;
-    p = new Node(value, top);
-    top = p;
-}
-
-template <typename Type>
-void LinkStack<Type>::clear()
-{
-    Node *p;
-
-    while (top)
-    {
-        p = top;
-        top = top->next;
-        delete p;
-    }
-}
-
-template <typename Type>
-int LinkStack<Type>::size() const
-{
-    int curlength = 0;
-    Node *p = top;
-    while (p)
-    {
-        ++curlength;
-        p = p->next;
-    }
-    return curlength;
-}
-
-template <typename Type>
-Type LinkStack<Type>::pop()
-{
-    Node *p;
-    p = top;
-    top = top->next;
-    return p->data;
-}
