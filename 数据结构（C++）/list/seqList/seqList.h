b79acdcfe45fe555011b0cfbde423a7e2ccec174
LWJ1608 <2194677100@qq.com>
2022-05-28 20:03:01 +0800

    push @2022年 05月 28日 星期六 20:03:01 CST

--- 数据结构（C++）/list/seqList/seqList.h
+++ 数据结构（C++）/list/seqList/seqList.h
@@ -26,7 +26,7 @@ public:
     bool isFull() const { return count == maxSize; } // 判满
     int size() const { return count; }               // 返回顺序表的当前存储元素的个数
     void insert(int i, const Type &value);           // 在位置i上插入一个元素value，表的长度增1
-    // void remove(int i);                              // 删除位置i上的元素value，若删除位置合法，表的长度减1
+    void remove(int i);                              // 删除位置i上的元素value，若删除位置合法，表的长度减1
     // int search(const Type &value) const;             // 查找值为value的元素第一次出现的位序
     // T visit(int i) const;                            // 访问位序为i的元素值，“位序”0表示第一个元素，类似于数组下标
     void resize();         // 扩容
