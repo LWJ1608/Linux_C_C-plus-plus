8324c2c679dd0547655c8aceb2d81ddc96ee2494
LWJ1608 <2194677100@qq.com>
2022-05-28 14:57:01 +0800

    push @2022年 05月 28日 星期六 14:57:01 CST

--- 数据结构（C++）/list/seqList/seqList.h
+++ 数据结构（C++）/list/seqList/seqList.h
@@ -18,18 +18,18 @@ private:
     int maxSize;   //数组容量
     void resize(); //扩容，因为只在当前文件中使用，所以放在私有成员中
 public:
-    SeqList(int isSize = MAXSIZE); //构造函数
-    SeqList(const SeqList &s);     //拷贝构造
-    ~SeqList();                    //析构函数
-    void clear() { curLength = 0; }               // 清空表，只需修改curLength
-    bool isEmpty() const { return curLength == 0; } // 判空
-    bool isFull() const {return }
-    int size() const { return curLength; }        // 返回顺序表的当前存储元素的个数
-    void insert(int i, const T &value);           // 在位置i上插入一个元素value，表的长度增1
-    void remove(int i);                           // 删除位置i上的元素value，若删除位置合法，表的长度减1
-    int search(const T &value) const;             // 查找值为value的元素第一次出现的位序
-    T visit(int i) const;                         // 访问位序为i的元素值，“位序”0表示第一个元素，类似于数组下标
-    void traverse() const;                        // 遍历顺序表
-    void inverse();                               // 逆置顺序表
+    SeqList(int isSize = MAXSIZE);                   //构造函数
+    SeqList(const SeqList &s);                       //拷贝构造
+    ~SeqList();                                      //析构函数
+    void clear() { count = 0; }                      // 清空表，只需修改count
+    bool isEmpty() const { return count == 0; }      // 判空
+    bool isFull() const { return count == maxSize; } //判满
+    int size() const { return count; }               // 返回顺序表的当前存储元素的个数
+    void insert(int i, const T &value);              // 在位置i上插入一个元素value，表的长度增1
+    void remove(int i);                              // 删除位置i上的元素value，若删除位置合法，表的长度减1
+    int search(const T &value) const;                // 查找值为value的元素第一次出现的位序
+    T visit(int i) const;                            // 访问位序为i的元素值，“位序”0表示第一个元素，类似于数组下标
+    void traverse() const;                           // 遍历顺序表
+    void inverse();                                  // 逆置顺序表
     bool Union(seqList<T> &B);
 };
