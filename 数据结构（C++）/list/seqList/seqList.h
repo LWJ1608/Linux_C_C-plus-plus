e445015df99f1a9cea6aa90b7ee51ec63f610083
LWJ1608 <2194677100@qq.com>
2022-05-28 16:32:01 +0800

    push @2022年 05月 28日 星期六 16:32:01 CST

--- 数据结构（C++）/list/seqList/seqList.h
+++ 数据结构（C++）/list/seqList/seqList.h
@@ -75,7 +75,7 @@ void SeqList<Type>::insert(int i, const Type &value) // 在位置i上插入一�
 template <typename Type>
 void SeqList<Type>::traverse() const // 遍历顺序表
 {
-    if (isEmpty)
+    if (isEmpty == nullptr)
     {
         std::cout << "数据为空！" << std::endl;
     }
