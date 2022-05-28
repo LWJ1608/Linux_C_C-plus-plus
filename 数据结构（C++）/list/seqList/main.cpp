446512b7d54c8509c977253620c6e35fac4de1e0
LWJ1608 <2194677100@qq.com>
2022-05-28 16:23:01 +0800

    push @2022年 05月 28日 星期六 16:23:01 CST

--- 数据结构（C++）/list/seqList/main.cpp
+++ 数据结构（C++）/list/seqList/main.cpp
@@ -7,8 +7,52 @@
  **/
 #include "seqList.h"
 
+void testSeqList()
+{
+    seqList<int> s1;
+    seqList<int> s2;
+
+    cout << "insert插入测试：" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        s1.insert(i, i);
+        s2.insert(i, 99);
+    }
+    s1.traverse();
+
+    cout << "测试返回表大小函数size（）：" << s1.size() << endl;
+
+    cout << "拷贝构造函数测试：" << endl;
+    // s2 = s1;
+    // s2.traverse();
+
+    // cout << "表满扩大容量函数resize():" << endl;
+    // cout << "未扩大表时的容量：" << s1.size() << endl;
+    // for (int i = 0; i < 10; i++)
+    //{
+    //	s1.insert(0, 88);
+    // }
+    // cout << "扩大表后容量为：" << s1.size() << endl;
+    // s1.traverse();
+
+    // cout << "测试删除元素函数remove()：" << endl;
+    // s1.remove(s1.size());
+    // s1.traverse();
+
+    // cout << "测试visit()功能：" << s1.visit(5) << endl;
+
+    // cout << "测试查找value元素功能：" << s1.search(4)<<" "<<s1.search(99)<< endl;
+
+    // cout << "测试逆置功能：" << endl;
+    // s1.inverse();
+    // s1.traverse();
+
+    cout << "测试s2表插入s1表：" << endl;
+    s1.Union(s2);
+    s1.traverse();
+}
 int main()
 {
-    
+
     return 0;
 }
