dd6c6c15068087143286fa6902334f7367675290
LWJ1608 <2194677100@qq.com>
2022-05-28 15:00:01 +0800

    push @2022年 05月 28日 星期六 15:00:01 CST

--- 数据结构（C++）/list/seqList/seqList.h
+++ 数据结构（C++）/list/seqList/seqList.h
@@ -13,17 +13,17 @@ template <typename Type>
 class SeqList
 {
 private:
-    Type *data;    //维护存储数据的指针
-    int count;     //记录元素个数
-    int maxSize;   //数组容量
-    void resize(); //扩容，因为只在当前文件中使用，所以放在私有成员中
+    Type *data;    // 维护存储数据的指针
+    int count;     // 记录元素个数
+    int maxSize;   // 数组容量
+    void resize(); // 扩容，因为只在当前文件中使用，所以放在私有成员中
 public:
-    SeqList(int isSize = MAXSIZE);                   //构造函数
-    SeqList(const SeqList &s);                       //拷贝构造
-    ~SeqList();                                      //析构函数
+    SeqList(int isSize = MAXSIZE);                   // 构造函数
+    SeqList(const SeqList &s);                       // 拷贝构造
+    ~SeqList();                                      // 析构函数
     void clear() { count = 0; }                      // 清空表，只需修改count
     bool isEmpty() const { return count == 0; }      // 判空
-    bool isFull() const { return count == maxSize; } //判满
+    bool isFull() const { return count == maxSize; } // 判满
     int size() const { return count; }               // 返回顺序表的当前存储元素的个数
     void insert(int i, const Type &value);           // 在位置i上插入一个元素value，表的长度增1
     void remove(int i);                              // 删除位置i上的元素value，若删除位置合法，表的长度减1
