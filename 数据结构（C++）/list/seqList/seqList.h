98ad7fb55a46a75968120f4cbef383364976d21d
LWJ1608 <2194677100@qq.com>
2022-05-28 13:09:01 +0800

    push @2022年 05月 28日 星期六 13:09:01 CST

--- 数据结构（C++）/list/seqList/seqList.h
+++ 数据结构（C++）/list/seqList/seqList.h
@@ -2,6 +2,6 @@
  * @Author: lwj
  * @Date: 2022-05-28 13:04:41
  * @LastEditTime: 2022-05-28 13:04:41
- * @Description:
+ * @Description:数据结构---顺序表
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/list/seqList/seqList.h
  **/
