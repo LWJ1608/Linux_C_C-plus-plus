8d83678eca53a415efd68aa305315d5ec830e741
LWJ1608 <2194677100@qq.com>
2022-05-28 20:23:01 +0800

    push @2022年 05月 28日 星期六 20:23:01 CST

--- 数据结构（C++）/list/seqList/seqList.h
+++ 数据结构（C++）/list/seqList/seqList.h
@@ -28,9 +28,9 @@ public:
     void insert(int i, const Type &value);           // 在位置i上插入一个元素value，表的长度增1
     void remove(int i);                              // 删除位置i上的元素value，若删除位置合法，表的长度减1
     int search(const Type &value) const;             // 查找值为value的元素第一次出现的位序
-    // T visit(int i) const;                            // 访问位序为i的元素值，“位序”0表示第一个元素，类似于数组下标
-    void resize();         // 扩容
-    void traverse() const; // 遍历顺序表
+    T visit(int i) const;                            // 访问位序为i的元素值，“位序”0表示第一个元素，类似于数组下标
+    void resize();                                   // 扩容
+    void traverse() const;                           // 遍历顺序表
     // void inverse();                                  // 逆置顺序表
     // bool Union(seqList<Type> &B);                     //合并两个表
 };
@@ -88,7 +88,10 @@ int SeqList<Type>::search(const Type &value) const // 查找值为value的元素
             return i;
     return -1; // 查找失败返回-1
 }
-// T visit(int i) const;                            // 访问位序为i的元素值，“位序”0表示第一个元素，类似于数组下标
+template <typename Type>
+T SeqList<Type>::visit(int i) const // 访问位序为i的元素值，“位序”0表示第一个元素，类似于数组下标
+{
+}
 template <typename Type>
 void SeqList<Type>::traverse() const // 遍历顺序表
 {
