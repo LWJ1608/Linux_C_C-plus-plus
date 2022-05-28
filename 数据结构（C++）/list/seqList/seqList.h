e8c5ca59fb3585e5112fc1248457e70dc658a827
LWJ1608 <2194677100@qq.com>
2022-05-28 15:14:01 +0800

    push @2022年 05月 28日 星期六 15:14:01 CST

--- 数据结构（C++）/list/seqList/seqList.h
+++ 数据结构（C++）/list/seqList/seqList.h
@@ -13,10 +13,9 @@ template <typename Type>
 class SeqList
 {
 private:
-    Type *data;    // 维护存储数据的指针
-    int count;     // 记录元素个数
-    int maxSize;   // 数组容量
-    void resize(); // 扩容，因为只在当前文件中使用，所以放在私有成员中
+    Type *data;  // 维护存储数据的指针
+    int count;   // 记录元素个数
+    int maxSize; // 数组容量
 public:
     SeqList(int isSize = MAXSIZE);                   // 构造函数
     SeqList(const SeqList &s);                       // 拷贝构造
@@ -29,6 +28,7 @@ public:
     void remove(int i);                              // 删除位置i上的元素value，若删除位置合法，表的长度减1
     int search(const Type &value) const;             // 查找值为value的元素第一次出现的位序
     T visit(int i) const;                            // 访问位序为i的元素值，“位序”0表示第一个元素，类似于数组下标
+    void resize();                                   // 扩容
     void traverse() const;                           // 遍历顺序表
     void inverse();                                  // 逆置顺序表
     bool Union(seqList<Type> &B);
