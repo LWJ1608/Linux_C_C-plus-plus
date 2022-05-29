9b6a35825e7a4de0ad129942b22e742cd49055c8
LWJ1608 <2194677100@qq.com>
2022-05-29 23:16:01 +0800

    push @2022年 05月 29日 星期日 23:16:01 CST

--- 数据结构（C++）/list/linkList/linkList.h
+++ 数据结构（C++）/list/linkList/linkList.h
@@ -87,67 +87,38 @@ bool LinkList<Type>::isEmpty() const // 判空
 template <typename Type>
 typename LinkList<Type>::Node *LinkList<Type>::getIndex(int i) const
 {
-    // i的范围[-1~count],-1是头结点的位置
-    // if (i < -1 || i > count - 1)
-    // {
-    //     return nullptr;
-    // }
-    // Node *tmp = head;
-    // int num = 0;
-    // while (num <= i)
-    // {
-    //     tmp = tmp->next;
-    //     num++;
-    // }
-    // return tmp;
-    Node *p = head;
+    i的范围[-1 ~count], -1是头结点的位置 if (i < -1 || i > count - 1)
+    {
+        return nullptr;
+    }
+    Node *tmp = head;
     int num = 0;
-
-    if (i < -1 || i > count - 1) // // getPosition可查找位置为[-1..n-1],若i为-1则定位到头结点,若i为0则定位到首元结点
-        return NULL;             // 当i非法时返回NULL
-
     while (num <= i)
     {
-        p = p->next;
+        tmp = tmp->next;
         num++;
     }
-    return p;
+    return tmp;
 }
 
 template <typename Type>
 void LinkList<Type>::insert(int i, const Type &value) // 在位置i上插入一个元素value，表的长度增1
 {
-    // if (i < 0 || i > count)
-    // {
-    //     std::cout << "位置错误！" << std::endl;
-    //     return;
-    // }
-
-    // Node *p, *q;
-    // p = getIndex(i - 1); // p是位序为i的结点的前驱
-    // q = new Node(value, p->next);
-    // p->next = q;
-
-    // if (p == tail)
-    // {
-    //     tail = q; // 插入点在表尾，插入结点成为新的尾结点
-    // }
-    // count++;
-    Node *p, *q;
-
-    if (i < 0 || i > count) // 合法的插入位置为[0..n]
+    if (i < 0 || i > count)
     {
         std::cout << "位置错误！" << std::endl;
         return;
     }
 
+    Node *p, *q;
     p = getIndex(i - 1); // p是位序为i的结点的前驱
     q = new Node(value, p->next);
     p->next = q;
 
     if (p == tail)
+    {
         tail = q; // 插入点在表尾，插入结点成为新的尾结点
-
+    }
     count++;
 }
 
