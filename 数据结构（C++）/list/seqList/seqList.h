bc80cec7f53f03b9326351610cf86895c67feebc
LWJ1608 <2194677100@qq.com>
2022-05-29 21:55:01 +0800

    push @2022年 05月 29日 星期日 21:55:01 CST

--- 数据结构（C++）/list/seqList/seqList.h
+++ 数据结构（C++）/list/seqList/seqList.h
@@ -60,7 +60,11 @@ SeqList<Type>::SeqList(const SeqList &s) // 拷贝构造
 template <typename Type>
 void SeqList<Type>::insert(int i, const Type &value) // 在位置i上插入一个元素value，表的长度增1
 {
-    assert(!isFull());           //断言.满了不能放
+    if (isEmpty())
+    {
+        cout << "已满！" << std::endl;
+        return;
+    }
     assert(i > 0 && i <= count); // i只能在[0~count]
     //在i处插入数据，i及其后面的元素向后移动一位
     for (int j = count; j > i; j--)
@@ -92,7 +96,6 @@ int SeqList<Type>::search(const Type &value) const // 查找值为value的元素
 template <typename Type>
 Type SeqList<Type>::visit(int i) const // 访问位序为i的元素值，“位序”0表示第一个元素，类似于数组下标
 {
-    assert(!isEmpty());              //断言
     assert(i > 0 && i <= count - 1); // i只能在[0~count-1]
     return data[i];
 }
