d07fe352488268a36ca184da577d11a675141d5c
LWJ1608 <2194677100@qq.com>
2022-05-29 22:18:01 +0800

    push @2022年 05月 29日 星期日 22:18:01 CST

--- 数据结构（C++）/list/linkList/main.cpp
+++ 数据结构（C++）/list/linkList/main.cpp
@@ -24,32 +24,32 @@ void testlinkList()
     L1.insert(2, 99);
     // L1.traverse();
 
-    cout << "测试判空和返回单链表大小：" << endl;
-    if (L1.isEmpty())
-    {
-        cout << "单链表为空！" << endl;
-    }
-    else
-    {
-        cout << "单链表不为空,其大小为：" << L1.size() << endl;
-    }
+    // cout << "测试判空和返回单链表大小：" << endl;
+    // if (L1.isEmpty())
+    // {
+    //     cout << "单链表为空！" << endl;
+    // }
+    // else
+    // {
+    //     cout << "单链表不为空,其大小为：" << L1.size() << endl;
+    // }
 
-    cout << "测试删除功能：" << endl;
-    L1.remove(2);
-    // L1.traverse();
+    // cout << "测试删除功能：" << endl;
+    // L1.remove(2);
+    // // L1.traverse();
 
-    cout << "测试查找位序为2的元素的值：" << endl;
-    // L1.clear();
-    cout << L1.visit(2) << endl;
+    // cout << "测试查找位序为2的元素的值：" << endl;
+    // // L1.clear();
+    // cout << L1.visit(2) << endl;
 
-    // cout << "头插法测试：" << endl;
-    // L1.headCreate();
-    // L1.traverse();
-    cout << "测试逆置操作：" << endl;
-    // L1.inverse();
-    // L1.traverse();
+    // // cout << "头插法测试：" << endl;
+    // // L1.headCreate();
+    // // L1.traverse();
+    // cout << "测试逆置操作：" << endl;
+    // // L1.inverse();
+    // // L1.traverse();
 
-    cout << "测试两个单链表合并操作：" << endl;
+    // cout << "测试两个单链表合并操作：" << endl;
     // L1.Union(&L2)->traverse();
 }
 
