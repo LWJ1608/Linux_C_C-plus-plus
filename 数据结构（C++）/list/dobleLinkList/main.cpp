4a70cadaf001e242c4e96824ea97dd57e3ab7c34
LWJ1608 <2194677100@qq.com>
2022-05-30 17:15:01 +0800

    push @2022年 05月 30日 星期一 17:15:01 CST

--- 数据结构（C++）/list/dobleLinkList/main.cpp
+++ 数据结构（C++）/list/dobleLinkList/main.cpp
@@ -1,18 +1,12 @@
 /**
  * @Author: lwj
- * @Date: 2022-05-30 14:44:24
- * @Description:
- * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/list/dobleLinkList/main.cpp
- **/
-/**
- * @Author: lwj
  * @Date: 2022-05-29 16:04:33
  * @Description:测试单链表
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/list/linkList/main.cpp
  **/
 #include <iostream>
-#include "linkList.h"
 using namespace std;
+#include "linkList.h"
 
 void testlinkList()
 {
@@ -20,47 +14,48 @@ void testlinkList()
     linkList<int> L2;
 
     cout << "插入测试：" << endl;
-    // for (int i = 0; i < 4; i++)
-    // {
-    L1.insert(1, 3);
-    L2.insert(1, 3);
-    // }
+    for (int i = 0; i < 4; i++)
+    {
+        L1.insert(i, i);
+        L2.insert(i, i);
+    }
 
-    // cout << "测试插入操作：" << endl;
-    // L1.insert(2, 99);
-    // // L1.traverse();
+    cout << "测试插入操作：" << endl;
+    L1.insert(2, 99);
+    L1.traverse();
 
-    // cout << "测试判空和返回单链表大小：" << endl;
-    // if (L1.isEmpty())
-    // {
-    //     cout << "单链表为空！" << endl;
-    // }
-    // else
-    // {
-    //     cout << "单链表不为空,其大小为：" << L1.size() << endl;
-    // }
+    cout << "测试判空和返回单链表大小：" << endl;
+    if (L1.empty())
+    {
+        cout << "单链表为空！" << endl;
+    }
+    else
+    {
+        cout << "单链表不为空,其大小为：" << L1.size() << endl;
+    }
 
-    // cout << "测试删除功能：" << endl;
-    // L1.remove(2);
-    // // L1.traverse();
+    cout << "测试删除功能：" << endl;
+    L1.remove(2);
+    L1.traverse();
 
-    // cout << "测试查找位序为2的元素的值：" << endl;
-    // // L1.clear();
-    // cout << L1.visit(2) << endl;
+    cout << "测试查找位序为2的元素的值：" << endl;
+    // L1.clear();
+    cout << L1.visit(2) << endl;
 
-    // // cout << "头插法测试：" << endl;
-    // // L1.headCreate();
-    // // L1.traverse();
-    // cout << "测试逆置操作：" << endl;
-    // // L1.inverse();
-    // // L1.traverse();
+    // cout << "头插法测试：" << endl;
+    // L1.headCreate();
+    // L1.traverse();
+    cout << "测试逆置操作：" << endl;
+    L1.inverse();
+    L1.traverse();
 
     // cout << "测试两个单链表合并操作：" << endl;
-    // L1.Union(&L2)->traverse();
+    // L1.Union(&L2);
+    // L1.traverse();
 }
 
 int main()
 {
-    testlinkList();
+    testlinkList(); //单链表测试
     return 0;
 }
