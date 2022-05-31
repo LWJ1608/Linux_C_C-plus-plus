d03c22543a5a0f1aab34e59d3abf1e59e9abf7ce
LWJ1608 <2194677100@qq.com>
2022-05-31 17:19:03 +0800

    push @2022年 05月 31日 星期二 17:19:03 CST

--- 数据结构（C++）/Queue/linkQueue/linkQueue.h
+++ 数据结构（C++）/Queue/linkQueue/linkQueue.h
@@ -1,5 +1,11 @@
 /**
  * @Author: lwj
+ * @Date: 2022-05-31 17:18:23
+ * @Description:
+ * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/Queue/linkQueue/linkQueue.h
+ **/
+/**
+ * @Author: lwj
  * @Date: 2022-05-31 17:18:22
  * @Description:
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/Queue/linkQueue/linkQueue.h
