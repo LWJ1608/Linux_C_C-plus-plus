3a8804c507a5266235bd4af9aed64928e7fbfbce
LWJ1608 <2194677100@qq.com>
2022-05-31 16:19:01 +0800

    push @2022年 05月 31日 星期二 16:19:01 CST

--- 数据结构（C++）/Queue/seqQueue/main.cpp
+++ 数据结构（C++）/Queue/seqQueue/main.cpp
@@ -1,6 +1,33 @@
 /**
  * @Author: lwj
  * @Date: 2022-05-31 15:16:14
- * @Description:测试顺序队列
+ * @Description:测试循环队列
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/Queue/seqQueue/main.cpp
  **/
+#include <iostream>
+using namespace std;
+#include "seqQueue.h"
+
+template <typename type>
+void testQuence(seqQueue<Type> &p)
+{
+    cout << "插入和取出测试：" << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        p.enQueue(i);
+    }
+    cout << "队列大小为：" << p.size() << endl;
+    while (!p.empty())
+    {
+        cout << p.deQueue() << " ";
+    }
+    cout << endl;
+    cout << "队首值为：" << p.getHead() << endl;
+}
+
+int main()
+{
+    seqQueue<int> s1;
+    testQuence(s1);
+    return 0;
+}
