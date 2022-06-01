5a1106c73760b7ff032362c89a7b0aba6dda9b11
LWJ1608 <2194677100@qq.com>
2022-06-01 22:40:02 +0800

    push @2022年 06月 01日 星期三 22:40:02 CST

--- 数据结构（C++）/String/String.h
+++ 数据结构（C++）/String/String.h
@@ -125,3 +125,7 @@ String &String::operator=(const String &str1) //重载=，把一个串的赋值
     strLength = str1.strLength;
     return *this;
 }
+inline char &String::operator[](size_t index) //重载[]，通过下标运算取出字符
+{
+    assert(index >= 0 && index < maxSize);
+}
