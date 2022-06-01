7db44bf65fd6f7e03bfa2a85171462873b827eb0
LWJ1608 <2194677100@qq.com>
2022-06-01 17:27:01 +0800

    push @2022年 06月 01日 星期三 17:27:01 CST

--- 数据结构（C++）/String/String.h
+++ 数据结构（C++）/String/String.h
@@ -30,7 +30,7 @@ private:
     int maxSize;   //最大容量
     int strLength; //记录串的长度
 public:
-    String(const char *str = nullptr);             //构造函数
+    String(const char *str = nullptr);          //构造函数
     String(const String &S);                    //拷贝构造
     ~String() { delete[] data; }                //析构函数
     size_t capacity() { return this->maxSize; } //返回串容量
@@ -40,6 +40,6 @@ String::String(const char *str)
 {
     maxSize = 2 * sizeof(str) / sizeof(str[0]);
     data = new char[maxSize + 1];
-    strncpy(data, maxSize, str);
     strLength = strlen(data);
+    strncpy(data, str, strLength);
 }
