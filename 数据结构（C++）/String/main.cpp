196cfcdaf6a88c85d5cc07346d92004338dc3689
LWJ1608 <2194677100@qq.com>
2022-06-01 19:32:01 +0800

    push @2022年 06月 01日 星期三 19:32:01 CST

--- 数据结构（C++）/String/main.cpp
+++ 数据结构（C++）/String/main.cpp
@@ -13,22 +13,23 @@ using namespace std;
 void test01()
 {
     //测试无参构造
-    String s1;
-    cout << "s1:maxSize = " << s1.capacity() << endl;
+    String a1;
+    cout << "s1:maxSize = " << a1.capacity() << endl;
     //测试有参构造
-    String s2("lwj");
-    cout << "s2:maxSize = " << s2.capacity() << endl;
+    String a2("lwj");
+    cout << "s2:maxSize = " << a2.capacity() << endl;
 }
 //测试重载函数
 void test02()
 {
     //重载==，用判断两个串是否相等
+    String ;
     //重载+，用于合并俩个串
     //重载=，把一个串的赋值
     //重载[]，通过下标运算取出字符
     //重载<<，用于输入串
     //重载>>，用于输出串
-    String s3;
+
 }
 int main()
 {
