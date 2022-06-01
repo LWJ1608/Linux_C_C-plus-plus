edcdcbfea9be1603ba7e42025a35dd4da39b54fc
LWJ1608 <2194677100@qq.com>
2022-06-01 16:42:01 +0800

    push @2022年 06月 01日 星期三 16:42:01 CST

--- 数据结构（C++）/String/String.h
+++ 数据结构（C++）/String/String.h
@@ -4,17 +4,6 @@
  * @Description:
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/String/string.h
  **/
-class String
-{
-private:
-    char *data;    //存储串
-    int maxSize;   //最大容量
-    int strLength; //记录串的长度
-public:
-    String(const char *s = nullptr);
-    String(const String &S);
-    ~String();
-};
 class outOfRange : public exception //用于检查范围的有效性
 {
 public:
@@ -31,3 +20,17 @@ public:
         return "ERROR! BAD SIZE.\n";
     }
 };
+class String
+{
+private:
+    char *data;    //存储串
+    int maxSize;   //最大容量
+    int strLength; //记录串的长度
+public:
+    String(const char *s = nullptr); //构造函数
+    String(const String &S);         //拷贝构造
+    ~String();                       //析构函数
+};
+String::String(const char *s)
+{
+}
