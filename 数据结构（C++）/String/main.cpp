b80aa329999d82faf8f7f10fc6f8ee9fa49c547e
LWJ1608 <2194677100@qq.com>
2022-06-01 17:12:01 +0800

    push @2022年 06月 01日 星期三 17:12:01 CST

--- 数据结构（C++）/String/main.cpp
+++ 数据结构（C++）/String/main.cpp
@@ -1,16 +1,17 @@
 /**
  * @Author: lwj
  * @Date: 2022-06-01 15:54:31
- * @Description:
+ * @Description:测试串的功能
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/String/main.cpp
  **/
 #include"String.h"
 #include <iostream>
 using namespace std;
 
+//测试构造函数功能
 void test01()
 {
-
+    
 }
 
 int main()
