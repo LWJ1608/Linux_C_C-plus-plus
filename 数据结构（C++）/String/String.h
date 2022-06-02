ca642c53c991a80a23630c7723d26bd1176ae194
LWJ1608 <2194677100@qq.com>
2022-06-02 18:38:01 +0800

    push @2022年 06月 02日 星期四 18:38:01 CST

--- 数据结构（C++）/String/String.h
+++ 数据结构（C++）/String/String.h
@@ -46,7 +46,7 @@ public:
     size_t size() const { return strLength; }                //求串的长度
     bool empty() const { return strLength == 0; }            //判空
     int compare(const String &str1) const;                   //比较当前串和串s的大小
-    String &subStr(int pos, const int num) const;            //从pos位置开始取长度为num的子串
+    String subStr(int pos, const int num) const;             //从pos位置开始取长度为num的子串
     //朴素的模式匹配算法
     //在pos位置插入子串s
     //删除从pos位置开始的num个字符
