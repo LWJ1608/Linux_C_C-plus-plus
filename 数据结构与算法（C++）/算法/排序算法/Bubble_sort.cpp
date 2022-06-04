3d8d81ca40b223f4652599e36519619452d19b1d
LWJ1608 <2194677100@qq.com>
2022-06-04 21:32:01 +0800

    push @2022年 06月 04日 星期六 21:32:01 CST

--- 数据结构与算法（C++）/算法/排序算法/Bubble_sort.cpp
+++ 数据结构与算法（C++）/算法/排序算法/Bubble_sort.cpp
@@ -19,7 +19,7 @@ template <typename Type>
 void bubbleSort(Type *arr, int size)
 {
     Type tmp;
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size - 1; i++)
     {
         for (int j = i + 1; j < size - i - 1; j++)
         {
