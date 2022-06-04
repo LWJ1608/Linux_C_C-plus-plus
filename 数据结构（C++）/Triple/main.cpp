db5b56529635345f26ff80123ac99aba4e77febc
LWJ1608 <2194677100@qq.com>
2022-06-04 20:12:01 +0800

    push @2022年 06月 04日 星期六 20:12:01 CST

---
+++ 数据结构（C++）/Triple/main.cpp
@@ -0,0 +1,6 @@
+/**
+ * @Author: lwj
+ * @Date: 2022-06-04 20:11:32
+ * @Description:
+ * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/Triple/main.cpp
+ **/
--- 数据结构（C++）/Triple/triple.h
+++ 数据结构（C++）/Triple/triple.h
@@ -1,6 +1,6 @@
 /**
  * @Author: lwj
  * @Date: 2022-06-04 20:09:36
- * @Description:
+ * @Description:稀疏矩阵
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/Triple/triple.h
  **/
