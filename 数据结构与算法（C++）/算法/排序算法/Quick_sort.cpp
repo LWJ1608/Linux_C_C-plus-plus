79f100a45d61bede1604dfe2b6ae77519160c068
LWJ1608 <2194677100@qq.com>
2022-06-04 22:22:01 +0800

    push @2022年 06月 04日 星期六 22:22:01 CST

--- 数据结构与算法（C++）/算法/排序算法/Quick_sort.cpp
+++ 数据结构与算法（C++）/算法/排序算法/Quick_sort.cpp
@@ -17,14 +17,15 @@ void print(const Type &arr, int size) //打印数组
 }
 
 template <typename Type>
-void quickSort(Type *arr, int size) //冒泡排序
+void quickSort(Type *arr, int size) //快速排序
 {
+
 }
 int main()
 {
     int arr[] = {4, 2, 5, 88, 4, 1, 88, 5};
     int count = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, count);
+    quickSort(arr, count);
     print(arr, count);
     return 0;
 }
