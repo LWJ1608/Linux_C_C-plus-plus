f618cf6d4f0661f4e40df07e3b8f33c54262d656
LWJ1608 <2194677100@qq.com>
2022-06-05 16:20:01 +0800

    push @2022年 06月 05日 星期日 16:20:01 CST

--- 数据结构与算法（C++）/算法/排序算法/Quick_sort.cpp
+++ 数据结构与算法（C++）/算法/排序算法/Quick_sort.cpp
@@ -38,7 +38,7 @@ int main()
 {
     int arr[] = {4, 2, 5, 88, 4, 1, 88, 5};
     int count = sizeof(arr) / sizeof(arr[0]);
-    partition(arr, count);
+    quickSort(arr, count);
     print(arr, count);
     return 0;
 }
