128afa882f91a9c117aa96fd2d32eef2573b8b24
LWJ1608 <2194677100@qq.com>
2022-06-07 14:09:01 +0800

    push @2022年 06月 07日 星期二 14:09:01 CST

--- 数据结构与算法（C++）/算法/排序算法/StraighSelectSort.cpp
+++ 数据结构与算法（C++）/算法/排序算法/StraighSelectSort.cpp
@@ -1,1 +1,24 @@
-
+#include<iostream>
+tempalate<typename Type>
+void print(Type arr[],int size)
+{
+	for(int i = 0;i < size;i++)
+	{
+		std::cout<<arr[i]<<" ";
+	}
+		std::cout<<std::endl;
+｝
+tempalate<typename Type>
+void straighSelectSort(Type arr[],int size)
+{
+	
+	
+}
+int main()
+{
+	int arr[] = {1,7,5,2,3,88,9};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	//StraighSelectSort(arr,size);
+	print(arr,size);
+	return 0;
+}
