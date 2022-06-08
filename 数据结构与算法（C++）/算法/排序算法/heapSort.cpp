c874fa9e9ab9f5438d705ee4db90a8b9dedae5bf
LWJ1608 <2194677100@qq.com>
2022-06-08 13:30:01 +0800

    push @2022年 06月 08日 星期三 13:30:01 CST

--- 数据结构与算法（C++）/算法/排序算法/heapSort.cpp
+++ 数据结构与算法（C++）/算法/排序算法/heapSort.cpp
@@ -4,3 +4,28 @@
  * @Description:堆排序
  * @FilePath: /Linux_C_C-plus-plus/数据结构与算法（C++）/算法/排序算法/heapSort.cpp
  **/
+#include <iostream>
+#include <algorithm>
+
+template <typename Type>
+void print(const Type *arr, const int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+template <typename Type>
+void straighSelectSort(Type *arr, const int size)
+{
+
+}
+int main()
+{
+    int arr[] = {1, 7, 5, 2, 3, 88, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    print(arr, size);
+    return 0;
+}
