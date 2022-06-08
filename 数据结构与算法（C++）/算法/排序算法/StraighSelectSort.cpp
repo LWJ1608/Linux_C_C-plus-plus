b4093028dd7bc48c2dec6b128012a4e41f99d48c
LWJ1608 <2194677100@qq.com>
2022-06-08 13:31:01 +0800

    push @2022年 06月 08日 星期三 13:31:01 CST

--- 数据结构与算法（C++）/算法/排序算法/StraighSelectSort.cpp
+++ 数据结构与算法（C++）/算法/排序算法/StraighSelectSort.cpp
@@ -1,39 +1,44 @@
-#include<iostream>
-#include<algorithm>
+/**
+ * @Author: lwj
+ * @Date: 2022-06-08 13:30:33
+ * @Description:
+ * @FilePath: /Linux_C_C-plus-plus/数据结构与算法（C++）/算法/排序算法/StraighSelectSort.cpp
+ **/
 
-template<typename Type>
-void print(const Type *arr,const int size)
+#include <iostream>
+#include <algorithm>
+template <typename Type>
+void print(const Type *arr, const int size)
 {
-	for(int i = 0;i < size;i++)
+	for (int i = 0; i < size; i++)
 	{
-		std::cout<<arr[i]<<" ";
+		std::cout << arr[i] << " ";
 	}
-		std::cout<<std::endl;
+	std::cout << std::endl;
 }
-template<typename Type>
-void straighSelectSort(Type *arr,const int size)
+template <typename Type>
+void straighSelectSort(Type *arr, const int size)
 {
 	Type min;
-	for(int i = 0;i < size - 1;i++)
+	for (int i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for(int j = i+1;j < size;j++)
+		for (int j = i + 1; j < size; j++)
 		{
-			if(arr[i] > arr[j])
+			if (arr[i] > arr[j])
 			{
 				min = j;
 			}
 		}
-		if(min != i) 
-		std::swap(arr[i],arr[min]);
+		if (min != i)
+			std::swap(arr[i], arr[min]);
 	}
-	
 }
 int main()
 {
-	int arr[] = {1,7,5,2,3,88,9};
-	int size = sizeof(arr)/sizeof(arr[0]);
-	straighSelectSort(arr,size);
-	print(arr,size);
+	int arr[] = {1, 7, 5, 2, 3, 88, 9};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	straighSelectSort(arr, size);
+	print(arr, size);
 	return 0;
 }
