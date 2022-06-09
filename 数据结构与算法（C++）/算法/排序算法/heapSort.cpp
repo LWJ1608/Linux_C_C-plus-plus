16a2a77376e3e6b55289f6205dab6bc0fdec7046
LWJ1608 <2194677100@qq.com>
2022-06-09 22:17:01 +0800

    push @2022年 06月 09日 星期四 22:17:01 CST

--- 数据结构与算法（C++）/算法/排序算法/heapSort.cpp
+++ 数据结构与算法（C++）/算法/排序算法/heapSort.cpp
@@ -21,9 +21,9 @@ void siftDown(Type arr[], int pos, int size)
 {
     int child;           //
     Type tmp = arr[pos]; //暂存根记录
-    for (; pos * 2 - 1 < size; pos = child)
+    for (; pos * 2 + 1 < size; pos = child)
     {
-        child = pos * 2 - 1;                                  // pos结点的左孩子
+        child = pos * 2 + 1;                                  // pos结点的左孩子
         if (child != size - 1 && arr[child + 1] > arr[child]) //选取两个孩子的大者
         {
             child++; //如果大于的话，选较大的那个，
