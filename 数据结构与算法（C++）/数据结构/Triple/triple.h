ab8e3337ac20d393ea9911312d522582817276da
LWJ1608 <2194677100@qq.com>
2022-06-12 14:47:04 +0800

    push @2022年 06月 12日 星期日 14:47:04 CST

--- 数据结构与算法（C++）/数据结构/Triple/triple.h
+++ 数据结构与算法（C++）/数据结构/Triple/triple.h
@@ -12,20 +12,22 @@ class Triple //三元组表
 private:
     struct Node //三元组表的类型，一个struct类型代表一个结点
     {
-        int row;                   //非零元素的行数
-        int col;                   //非零元素的列数
-        Type data;                 //非零元素的值
+        int row;                                 //非零元素的行数
+        int col;                                 //非零元素的列数
+        Type data;                               //非零元素的值
         Node(int r = -1, int c = -1, Type d = 0) //结点构造函数
         {
             row = r;
             col = c;
             data = d;
         }
-        Node(int r, int c, Type d)//非零元素结点
+        Node(int r, int c, Type d) //非零元素结点
         {
             row = r;
             col = c;
             data = d;
         }
     };
+
+public:
 };
