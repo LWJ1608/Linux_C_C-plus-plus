986d0a2b19f5f2007455dc04f4c2d2140f0e1310
LWJ1608 <2194677100@qq.com>
2022-06-12 14:39:04 +0800

    push @2022年 06月 12日 星期日 14:39:04 CST

--- 数据结构与算法（C++）/数据结构/Triple/triple.h
+++ 数据结构与算法（C++）/数据结构/Triple/triple.h
@@ -1,17 +1,19 @@
 /**
  * @Author: lwj
  * @Date: 2022-06-04 20:09:36
- * @Description:稀疏矩阵
+ * @Description:三元组表构建稀疏矩阵
  * @FilePath: /Linux_C_C-plus-plus/数据结构（C++）/Triple/triple.h
  **/
 #include <iostream>
 
 template <typename Type>
-class Triple
+class Triple //三元组表
 {
 private:
-    struct Node
+    struct Node //三元组表的类型，一个struct类型代表一个结点
     {
-        int row
+        int row;  //非零元素的行数
+        int col;  //非零元素的列数
+        int data; //非零元素的值
     };
 };
