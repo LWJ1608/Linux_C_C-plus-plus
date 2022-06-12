d2b5f5925309ca9ebcebe9ecb36e628867288b93
LWJ1608 <2194677100@qq.com>
2022-06-12 15:02:04 +0800

    push @2022年 06月 12日 星期日 15:02:04 CST

--- 数据结构与算法（C++）/数据结构/Triple/triple.h
+++ 数据结构与算法（C++）/数据结构/Triple/triple.h
@@ -21,7 +21,7 @@ private:
             col = c;
             data = d;
         }
-        void setNodeValue(int r, int c, Type d) //元素赋值
+        void pushNodeValue(int r, int c, Type d) //元素赋值
         {
             row = r;
             col = c;
@@ -34,8 +34,7 @@ private:
     int count;    //非零元素个数
     int maxSize;  //矩阵容量
 public:
-    Triple(int r, int c, int size = 10); //构造函数，默认矩阵容量为10
-    ~Triple() { delete[] maxSize; }      //析构函数
-    void pushValue
-        set
+    Triple(int r, int c, int size = 10);    //构造函数，默认矩阵容量为10
+    ~Triple() { delete[] maxSize; }         //析构函数
+    void pushValue(int r, int c, int size); //元素赋值
 };
