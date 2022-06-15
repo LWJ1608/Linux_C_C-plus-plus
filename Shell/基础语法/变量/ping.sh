19b380f82c491729b4a845e131ba9fb6050bb408
LWJ1608 <2194677100@qq.com>
2022-06-15 19:31:04 +0800

    push @2022年 06月 15日 星期三 19:31:04 CST

--- Shell/基础语法/变量/ping.sh
+++ Shell/基础语法/变量/ping.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 ##ping 命令的使用
+#从键盘读入变量
+read -p "please input a ip: " ip
 
-ip=www.baidu.com
-if[ ping -c1 $ip &>/dev/null ]; then
+#ip=www.baidu.com
+ping -c2 $ip &>/dev/null
+if [ $? -eq 0 ] &>/dev/null; then
 	echo "$ip is up"
 else
 	echo "$ip is down"
