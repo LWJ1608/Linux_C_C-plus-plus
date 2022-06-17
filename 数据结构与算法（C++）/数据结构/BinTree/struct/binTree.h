caf30819371315da722700974eacfa811a93c810
LWJ1608 <2194677100@qq.com>
2022-06-17 16:21:07 +0800

    push @2022年 06月 17日 星期五 16:21:07 CST

--- 数据结构与算法（C++）/数据结构/BinTree/struct/binTree.h
+++ 数据结构与算法（C++）/数据结构/BinTree/struct/binTree.h
@@ -120,8 +120,8 @@ int BinTree<Type>::leafNum(Node *t) const //返回二叉树叶子数
 template <typename Type>
 void BinTree<Type>::preOrderCreate(Type flag, Node *&t) //前序法创建二叉树
 {
-    Type value;   //键盘输入的值
-    cin >> value; //输入
+    Type value;        //键盘输入的值
+    std::cin >> value; //输入
     if (value != flag)
     {
         root = new Node(value);
