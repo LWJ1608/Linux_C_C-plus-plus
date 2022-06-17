4576333d9e44ccf290d5938d5ccfcfb18a11aef6
LWJ1608 <2194677100@qq.com>
2022-06-17 15:15:04 +0800

    push @2022年 06月 17日 星期五 15:15:04 CST

--- 数据结构与算法（C++）/数据结构/BinTree/struct/binTree.h
+++ 数据结构与算法（C++）/数据结构/BinTree/struct/binTree.h
@@ -47,15 +47,8 @@ private: //以下为内部接口
 public:
     BinTree() : root(nullptr) {}
     ~BinTree() { clear(); }
-    bool empty() const { return root == nullptr; } //判空
-    void clear()
-    {
-        if (root)
-        {
-            clear(t);
-        }
-        root = nullptr;
-    }                                                 //清空
+    bool empty() const { return root == nullptr; }    //判空
+    void clear() { if (root){clear(root);}root = nullptr; } //清空
     int size() const;                                 //返回返回结点个数
     int hight() const;                                //返回二叉树高度
     int leafNum() const;                              //返回二叉树叶子数
