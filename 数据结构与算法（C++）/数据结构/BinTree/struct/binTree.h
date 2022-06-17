e02eb94978b56d7523c53a9f08bbb57a0caf2a14
LWJ1608 <2194677100@qq.com>
2022-06-17 19:58:04 +0800

    push @2022年 06月 17日 星期五 19:58:04 CST

--- 数据结构与算法（C++）/数据结构/BinTree/struct/binTree.h
+++ 数据结构与算法（C++）/数据结构/BinTree/struct/binTree.h
@@ -18,8 +18,15 @@ private:
         Type data;                                          //数据
         Node *leftChild, *rightChild;                       //分别为左孩子和右孩子
         Node() : leftChild(nullptr), rightChild(nullptr) {} //无参构造
-        Node(Type value, Node *left = nullptr, Node *rigth = nullptr)
-            : data(value), leftChild(left), rightChild(rigth) {} //有参构造
+        // Node(Type value, Node *left = nullptr, Node *rigth = nullptr)
+        //     : data(value), leftChild(left), rightChild(rigth) {} //有参构造
+        Node(Type value, Node *left = nullptr, Node *rigth = nullptr) //有参构造
+        {
+            data = value;
+            leftChild = left;
+            rightChild = rigth;
+        }
+
         ~Node(){};
     };
     Node *root; //指向二叉树的根
