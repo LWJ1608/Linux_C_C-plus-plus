4885729584fc3a67a84759a3d72de309e0c1c98d
LWJ1608 <2194677100@qq.com>
2022-06-17 10:49:05 +0800

    push @2022年 06月 17日 星期五 10:49:05 CST

--- 数据结构与算法（C++）/数据结构/BinTree/class/binTree.h
+++ 数据结构与算法（C++）/数据结构/BinTree/class/binTree.h
@@ -1,13 +1,7 @@
 /**
  * @Author: lwj
- * @Date: 2022-06-17 10:43:18
- * @Description:
- * @FilePath: /Linux_C_C-plus-plus/数据结构与算法（C++）/数据结构/BinTree/class/binTree.h
- **/
-/**
- * @Author: lwj
  * @Date: 2022-06-17 09:10:37
- * @Description:二叉树方法的实现
+ * @Description:二叉树方法的实现(结点类型是类实现)
  * @FilePath: /Linux_C_C-plus-plus/数据结构与算法（C++）/数据结构/BinTree/binTree.h
  **/
 #pragma once
