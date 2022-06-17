2269154e62e677defb50fb12587679baf1c4653d
LWJ1608 <2194677100@qq.com>
2022-06-17 20:52:04 +0800

    push @2022年 06月 17日 星期五 20:52:04 CST

--- 数据结构与算法（C++）/数据结构/BinTree/struct/main.cpp
+++ 数据结构与算法（C++）/数据结构/BinTree/struct/main.cpp
@@ -16,7 +16,7 @@ void test01()
     cout << "二叉树结点个数为：" << t1.size() << endl;
     cout << "二叉树高度为：" << t1.height() << endl;
     cout << "二叉树叶子数为：" << t1.leafNum() << endl;
-    cout << "前序遍历二叉树："
+    cout << "前序遍历二叉树：";
 }
 
 int main()
