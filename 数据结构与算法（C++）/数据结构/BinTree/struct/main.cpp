96fee713f346df7fad4e75244f6a5dac79bd5dcb
LWJ1608 <2194677100@qq.com>
2022-06-18 09:46:04 +0800

    push @2022年 06月 18日 星期六 09:46:04 CST

--- 数据结构与算法（C++）/数据结构/BinTree/struct/main.cpp
+++ 数据结构与算法（C++）/数据结构/BinTree/struct/main.cpp
@@ -17,8 +17,8 @@ void test01()
     cout << "二叉树高度为：" << t1.height() << endl;
     cout << "二叉树叶子数为：" << t1.leafNum() << endl;
     cout << "root结点的孩子的值分别为：\n"
-         << "leftChild:" << *t1.findLeft() <<"\n"
-         <<"rightChild:"<<*t1.findRight();
+         << "leftChild:" << t1.findLeft()->data <<"\n"
+         <<"rightChild:"<<t1.findRight()->data;
     "前序遍历二叉树：";
     t1.preOrderTraverse();
     cout << "\n中序遍历二叉树：";
