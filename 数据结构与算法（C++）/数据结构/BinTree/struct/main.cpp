31f68f6002a4966d63d739610508808fc2dea786
LWJ1608 <2194677100@qq.com>
2022-06-23 13:13:06 +0800

    push @2022年 06月 23日 星期四 13:13:06 CST

--- 数据结构与算法（C++）/数据结构/BinTree/struct/main.cpp
+++ 数据结构与算法（C++）/数据结构/BinTree/struct/main.cpp
@@ -16,7 +16,7 @@ void test01()
     cout << "二叉树结点个数为：" << t1.size() << endl;
     cout << "二叉树高度为：" << t1.height() << endl;
     cout << "二叉树叶子数为：" << t1.leafNum() << endl;
-    t1.findI('c');
+    Node<char> *iNode = t1.findI('c');
     cout << "t1.findI('c')->data = " << t1.findI('c')->data << endl;
     cout << "元素c的父结点：" << t1.findParent(t1.findI('c'))->data << endl;
     cout << "前序遍历二叉树：";
