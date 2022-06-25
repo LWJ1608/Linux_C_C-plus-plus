d36ac6270f13d2a82773d8b10ecd9f906fe1e64d
LWJ1608 <2194677100@qq.com>
2022-06-25 20:45:04 +0800

    push @2022年 06月 25日 星期六 20:45:04 CST

--- 数据结构与算法（C++）/数据结构/BinTree/struct版/test.cpp
+++ 数据结构与算法（C++）/数据结构/BinTree/struct版/test.cpp
@@ -6,7 +6,3 @@
  * @FilePath: /Linux_C_C-plus-plus/数据结构与算法（C++）/数据结构/BinTree/struct版/test.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-/**
- * @description:
- * @return {*}
- */
