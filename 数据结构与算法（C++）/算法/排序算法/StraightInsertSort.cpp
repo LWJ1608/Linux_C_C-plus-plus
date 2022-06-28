7e25ad2fe156917fdbc0338999cad1bbb2db9435
LWJ1608 <2194677100@qq.com>
2022-06-28 20:46:04 +0800

    push @2022年 06月 28日 星期二 20:46:04 CST

--- 数据结构与算法（C++）/算法/排序算法/StraightInsertSort.cpp
+++ 数据结构与算法（C++）/算法/排序算法/StraightInsertSort.cpp
@@ -2,7 +2,13 @@
  * @Author: lwj
  * @Date: 2022-06-28 20:43:29
  * @FilePath: /Linux_C_C-plus-plus/数据结构与算法（C++）/算法/排序算法/StraightInsertSort.cpp
- * @Description:
+ * @Description:直接插入排序
  **/
 
-#include
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
