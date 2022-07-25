79dbb6aab76d56bf36a596c11cb4141c4f3817ab
LWJ1608 <2194677100@qq.com>
2022-07-25 16:00:06 +0800

    push @2022年 07月 25日 星期一 16:00:06 CST

--- test01/MyArray.hpp
+++ test01/MyArray.hpp
@@ -8,10 +8,10 @@ private:
     int mSize;    //元素个数
     int T *Addr;  //元素首地址
 public:
-    MyArry(int capcity);       //构造函数
-    MyArry(const MyArry &arr); //拷贝构造
-    T &operator[](const int index);
-    MyArry<T> operator=(const MyArry &arr) void pushBack(const T &data);
+    MyArry(int capcity);                                                 //构造函数
+    MyArry(const MyArry &arr);                                           //拷贝构造
+    T &operator[](const int index);                                      // []号重载
+    MyArry<T> operator=(const MyArry &arr) void pushBack(const T &data); // =号重载
 };
 
 template <typename T>
@@ -29,6 +29,12 @@ MyArry<T>::MyArry(const MyArry &arr)
     this->mSize = arr->mSize;
     T *arr1 = new T[mCapcity];
     for (int i = 0; i < mSize; i++)
+    {
+        arr1[i] = arr[i];
+    }
 }
 
 template <typename T>
+typename T &operator[](const int index) // []号重载
+{
+}
