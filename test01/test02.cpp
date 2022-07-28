adedf40e97da33b55898f6a0eb293adf39f65383
LWJ1608 <2194677100@qq.com>
2022-07-28 11:54:04 +0800

    push @2022年 07月 28日 星期四 11:54:04 CST

--- test01/test02.cpp
+++ test01/test02.cpp
@@ -2,5 +2,18 @@
  * @Author: lwj
  * @Date: 2022-07-28 11:48:57
  * @FilePath: /Linux_C_C-plus-plus/test01/test02.cpp
- * @Description:
+ * @Description:文件读写
  **/
+#include <iostream>
+using namespace std;
+#include <fstream>
+
+void test02()
+{
+}
+
+int main()
+{
+    test02();
+    return 0;
+}
