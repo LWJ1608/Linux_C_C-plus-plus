12649384cd95ef4dfc4b340869741274c94ec023
LWJ1608 <2194677100@qq.com>
2022-08-29 10:47:05 +0800

    push @2022年 08月 29日 星期一 10:47:05 CST

--- test01/deque.cpp
+++ test01/deque.cpp
@@ -10,13 +10,30 @@
 
 void printDeque(std::deque<int> v)
 {
-    for (std::deque<int> it = v.begin(); it)
+    for (std::deque<int>::iterator it = v.begin(); it != v.end(); it++)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
 }
+// deque构造函数
 void test01()
 {
+    //默认构造函数
+    std::deque<int> v1;
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_back(4);
+
+    //区间拷贝构造
+    std::deque<int> v2(v1.begin(), v1.end());
+    //拷贝n个数
+    std::deque<int> v3(1, v1.begin());
 }
 int main()
 {
 
+    test01();
     return 0;
 }
