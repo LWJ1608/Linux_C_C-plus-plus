a469460f02501c842eb4cf9616971031ea46ea09
LWJ1608 <2194677100@qq.com>
2022-08-29 15:06:04 +0800

    push @2022年 08月 29日 星期一 15:06:04 CST

--- test01/set.cpp
+++ test01/set.cpp
@@ -1,15 +1,15 @@
-/** 
+/**
  * @Author: lwj
  * @Date: 2022-08-29 14:59:43
  * @FilePath: /Linux_C_C-plus-plus/test01/set.cpp
  * @Description: set
  **/
-#include<iostrea>
-#include<set>
+#include <iostream>
+#include <set>
 class compare
 {
 public:
-    bool operator()(int s1,int s2)
+    bool operator()(int s1, int s2)
     {
         return s1 < s2;
     }
@@ -18,6 +18,5 @@ public:
 int main()
 {
     std::set<int> v1;
-    v1.
-    return 0;
+    v1.return 0;
 }
