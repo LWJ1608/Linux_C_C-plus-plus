01a9049060bfdae06f2a929a78bfb93024f282ef
LWJ1608 <2194677100@qq.com>
2022-08-29 10:36:04 +0800

    push @2022年 08月 29日 星期一 10:36:04 CST

--- test01/deque.cpp
+++ test01/deque.cpp
@@ -2,5 +2,21 @@
  * @Author: lwj
  * @Date: 2022-07-27 12:32:33
  * @FilePath: /Linux_C_C-plus-plus/test01/test01.cpp
- * @Description:
+ * @Description:deque(双端数组)的使用
  **/
+
+#include <iostream>
+#include <deque>
+
+void printDeque(std::deque<int> v)
+{
+    for (std::deque<int> it = v.begin(); it)
+}
+void test01()
+{
+}
+int main()
+{
+
+    return 0;
+}
