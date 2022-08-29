c09dcf5bb432612dca72a090628e6f88d2c41f6c
LWJ1608 <2194677100@qq.com>
2022-08-29 15:28:05 +0800

    push @2022年 08月 29日 星期一 15:28:05 CST

--- test01/stack.cpp
+++ test01/stack.cpp
@@ -1,24 +1,26 @@
-/** 
+/**
  * @Author: lwj
  * @Date: 2022-08-29 14:12:10
  * @FilePath: /Linux_C_C-plus-plus/test01/stack.cpp
  * @Description: stack(栈)的使用
  **/
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 
 class Person
 {
 private:
     int m_id;
+
 public:
-    Person(int id):m_id(id){}
+    Person(int id) : m_id(id) {}
 };
 void test01()
 {
-    Person* p1;
+    Person *p1;
     Person p(10491919);
-    std::stack<Person> ;
+    p1 = p;
+    std::stack<Person>;
     P.push(9);
 }
 int main()
