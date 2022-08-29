2a395d3d1238f07ac6443608c9ff62432a50ad8a
LWJ1608 <2194677100@qq.com>
2022-08-29 14:19:05 +0800

    push @2022年 08月 29日 星期一 14:19:05 CST

--- test01/stack.cpp
+++ test01/stack.cpp
@@ -2,5 +2,27 @@
  * @Author: lwj
  * @Date: 2022-08-29 14:12:10
  * @FilePath: /Linux_C_C-plus-plus/test01/stack.cpp
- * @Description: 
+ * @Description: stack(栈)的使用
  **/
+#include<iostream>
+#include<stack>
+
+class Person
+{
+private:
+    int m_id;
+public:
+    Person(int id):m_id(id){}
+};
+void test01()
+{
+    Person* p1;
+    Person p(10491919);
+    stack<Person*> P[2];
+    
+}
+int main()
+{
+
+    return 0;
+}
