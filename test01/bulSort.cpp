8c53310c60ff8e0a797d8d9b7685f9c41d6177a4
LWJ1608 <2194677100@qq.com>
2022-08-29 20:23:04 +0800

    push @2022年 08月 29日 星期一 20:23:04 CST

--- test01/bulSort.cpp
+++ test01/bulSort.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
+void printArr(int arr[], int count);
 template <typename T>
-void bubbleSort(T arr[], )
+void bubbleSort(T arr[], int count)
 {
+    bool flag = false;
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < ; ++)
+        {
+        }
+    }
 }
 int main()
 {
-
+    int arr[] = {2, 4, 12, 7, 8, 4};
+    int size = sizeof(arr) / sizeof(arr[1]);
+    bubbleSort(arr, size);
     return 0;
 }
