626742511bf45f36320d5a7300cfc4d34112e9dc
LWJ1608 <2194677100@qq.com>
2022-08-29 20:27:06 +0800

    push @2022年 08月 29日 星期一 20:27:06 CST

--- test01/bulSort.cpp
+++ test01/bulSort.cpp
@@ -14,7 +14,7 @@ void bubbleSort(T arr[], int count)
     bool flag = false;
     for (int i = 0; i < count; i++)
     {
-        for (int j = i + 1; j < count - 1 - i; ++)
+        for (int j = i + 1; j < count - 1 - i; j++)
         {
             if ()
         }
