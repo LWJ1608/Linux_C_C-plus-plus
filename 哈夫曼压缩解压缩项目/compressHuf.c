fd60ca5d3bdb791bcfcc7a560bab28a644ce9fa4
LWJ1608 <2194677100@qq.com>
2022-08-29 19:58:04 +0800

    push @2022年 08月 29日 星期一 19:58:04 CST

--- 哈夫曼压缩解压缩项目/compressHuf.c
+++ 哈夫曼压缩解压缩项目/compressHuf.c
@@ -1,78 +1,88 @@
 #pragma pack(push)
 #pragma pack(1)
 
-//Ñ¹ËõÔ´³ÌĞò
+//å‹ç¼©æºç¨‹åº
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
 
-#define		TRUE		1
-#define		FALSE		0
+#define TRUE 1
+#define FALSE 0
 
-#define		NOT_FOUND	-1
+#define NOT_FOUND -1
 
-#define		TARGET_FILE_EXT		".Mechuf"
+#define TARGET_FILE_EXT ".Mechuf"
 
-#define		SET_BIT(byte, i)	(byte |= 1 << (7 ^ (i)))  //ÉèÖÃÕâ¸ö×Ö½ÚµÄÖ¸¶¨Î»Îª1
-#define		CLR_BIT(byte, i)	(byte &= ~(1 << (7 ^ (i)))) //ÉèÖÃÕâ¸ö×Ö½ÚµÄÖ¸¶¨Î»Îª0
+#define SET_BIT(byte, i) (byte |= 1 << (7 ^ (i)))	 //è®¾ç½®è¿™ä¸ªå­—èŠ‚çš„æŒ‡å®šä½ä¸º1
+#define CLR_BIT(byte, i) (byte &= ~(1 << (7 ^ (i)))) //è®¾ç½®è¿™ä¸ªå­—èŠ‚çš„æŒ‡å®šä½ä¸º0
 
-typedef unsigned char	u8;
-typedef u8	boolean;
-typedef unsigned int	u32;
+typedef unsigned char u8;
+typedef u8 boolean;
+typedef unsigned int u32;
 
-typedef struct FREQ{  //Í³¼Æ×Ö·ûÖÖÀà¼°Æµ¶È
-	u8 alpha;  //×Ö·ûÖÖÀà,0-255
-	u32	freq;  //×Ö·ûÆµ¶È
-}FREQ;
+typedef struct FREQ
+{			  //ç»Ÿè®¡å­—ç¬¦ç§ç±»åŠé¢‘åº¦
+	u8 alpha; //å­—ç¬¦ç§ç±»,0-255
+	u32 freq; //å­—ç¬¦é¢‘åº¦
+} FREQ;
 
-typedef struct HUFF_TABLE{  //¹ş·òÂü±í
-	FREQ freq;   //×Ö·ûÖÖÀà¼°Æµ¶È
-	short leftChild;  //×óº¢×ÓÏÂ±ê
-	short rightChild; //ÓÒº¢×ÓÏÂ±ê
-	u8	isVisited;    //ÊÇ·ñ·ÃÎÊµÄ±ê¼Ç
-	char *huffCode;   //¹ş·òÂü±àÂë
-}HUFF_TABLE;
+typedef struct HUFF_TABLE
+{					  //å“ˆå¤«æ›¼è¡¨
+	FREQ freq;		  //å­—ç¬¦ç§ç±»åŠé¢‘åº¦
+	short leftChild;  //å·¦å­©å­ä¸‹æ ‡
+	short rightChild; //å³å­©å­ä¸‹æ ‡
+	u8 isVisited;	  //æ˜¯å¦è®¿é—®çš„æ ‡è®°
+	char *huffCode;	  //å“ˆå¤«æ›¼ç¼–ç 
+} HUFF_TABLE;
 
-typedef struct MECHUF_HEAD{
-	char flag[6];				//ÎÄ¼ş±êÊ¶
-	u8 alphaCount;				//×Ö·ûÖÖÀà¸öÊı alphaCount*5 ×Ö·û¼°Æä³öÏÖÆµ¶ÈµÄÊı¾İ¿é´óĞ¡
-	u32 bitsCount;				//Ñ¹Ëõ½á¹û·¶Î§ 
-}MECHUF_HEAD;
+typedef struct MECHUF_HEAD
+{
+	char flag[6];  //æ–‡ä»¶æ ‡è¯†
+	u8 alphaCount; //å­—ç¬¦ç§ç±»ä¸ªæ•° alphaCount*5 å­—ç¬¦åŠå…¶å‡ºç°é¢‘åº¦çš„æ•°æ®å—å¤§å°
+	u32 bitsCount; //å‹ç¼©ç»“æœèŒƒå›´
+} MECHUF_HEAD;
 
 int getFreq(char *fileName, FREQ **freq);
 void showFreq(const FREQ *freq, int alphaCount);
-HUFF_TABLE *initHuffTab(FREQ *freq, int alphaCount);  //³õÊ¼»¯¹ş·òÂü±í(Ò¶×Ó½áµã)
+HUFF_TABLE *initHuffTab(FREQ *freq, int alphaCount); //åˆå§‹åŒ–å“ˆå¤«æ›¼è¡¨(å¶å­ç»“ç‚¹)
 void destoryHuffTab(HUFF_TABLE **huf, int alphaCount);
 void showHuffTab(HUFF_TABLE *huf, int alphaCount);
 int findMinFreqIndex(HUFF_TABLE *huf, int count);
 void makeHuffTree(HUFF_TABLE *huf, int count);
 void makeHuffCode(HUFF_TABLE *huf, int root, char *str, int index);
 void getTargetFileName(char *, char *);
-void coddingFile(HUFF_TABLE *, FREQ *, int , char *, char *);
+void coddingFile(HUFF_TABLE *, FREQ *, int, char *, char *);
 void setIndexOfCh(HUFF_TABLE *huf, int alphaCount, short *ar);
 void encoddingFile(HUFF_TABLE *huf, int alphaCount, char *tgFile);
 
-void encoddingFile(HUFF_TABLE *huf, int alphaCount, char *tgFile){
+void encoddingFile(HUFF_TABLE *huf, int alphaCount, char *tgFile)
+{
 	FILE *fp;
 	FILE *fpout;
 	int ch;
-	int root = 2*(alphaCount-1);
+	int root = 2 * (alphaCount - 1);
 
-	if((fp = fopen(tgFile, "r")) == NULL){
-		printf("Ã»ÓĞÄ¿±êÎÄ¼ş:%s£¡\n", tgFile);
+	if ((fp = fopen(tgFile, "r")) == NULL)
+	{
+		printf("æ²¡æœ‰ç›®æ ‡æ–‡ä»¶:%sï¼\n", tgFile);
 		return;
 	}
-	if((fpout = fopen("zhomecaaa.txt", "w")) == NULL){
+	if ((fpout = fopen("zhomecaaa.txt", "w")) == NULL)
+	{
 		return;
 	}
 
-	while(!feof(fp)){
-		if(huf[root].leftChild != -1){
+	while (!feof(fp))
+	{
+		if (huf[root].leftChild != -1)
+		{
 			ch = fgetc(fp);
 			root = ch == '0' ? huf[root].leftChild : huf[root].rightChild;
-		}else{
+		}
+		else
+		{
 			fputc(huf[root].freq.alpha & 0x000000FF, fpout);
-			root = 2*(alphaCount-1);
+			root = 2 * (alphaCount - 1);
 		}
 	}
 
@@ -80,33 +90,38 @@ void encoddingFile(HUFF_TABLE *huf, int alphaCount, char *tgFile){
 	fclose(fpout);
 }
 
-void setIndexOfCh(HUFF_TABLE *huf, int alphaCount, short *ar){
+void setIndexOfCh(HUFF_TABLE *huf, int alphaCount, short *ar)
+{
 	int i;
 
-	for(i = 0; i < alphaCount; i++){
+	for (i = 0; i < alphaCount; i++)
+	{
 		ar[huf[i].freq.alpha] = i;
 	}
 }
 
-void coddingFile(HUFF_TABLE *huf, FREQ *freq, int alphaCount, char *scFile, char *tgFile){
+void coddingFile(HUFF_TABLE *huf, FREQ *freq, int alphaCount, char *scFile, char *tgFile)
+{
 	FILE *fpin;
 	FILE *fpout;
 	int ch;
 	short indexOfch[256] = {0};
-	MECHUF_HEAD head = {'M','E','C','H','U','F', 0};
+	MECHUF_HEAD head = {'M', 'E', 'C', 'H', 'U', 'F', 0};
 	u8 hCode;
 	u8 bitIndex = 0;
 	u8 huffCodeIndex;
 	u32 bitsCount = 0;
-		
+
 	setIndexOfCh(huf, alphaCount, indexOfch);
-	if((fpin = fopen(scFile, "r")) == NULL){
-		printf("%sÎÄ¼ş´ò¿ªÊ§°Ü£¡£¡£¡", scFile);
+	if ((fpin = fopen(scFile, "r")) == NULL)
+	{
+		printf("%sæ–‡ä»¶æ‰“å¼€å¤±è´¥ï¼ï¼ï¼", scFile);
 		return;
 	}
-	
-	if((fpout = fopen(tgFile, "w+b")) == NULL){
-		printf("%sÎÄ¼ş´´½¨Ê§°Ü£¡£¡£¡", tgFile);
+
+	if ((fpout = fopen(tgFile, "w+b")) == NULL)
+	{
+		printf("%sæ–‡ä»¶åˆ›å»ºå¤±è´¥ï¼ï¼ï¼", tgFile);
 		return;
 	}
 
@@ -114,14 +129,20 @@ void coddingFile(HUFF_TABLE *huf, FREQ *freq, int alphaCount, char *scFile, char
 	fwrite(freq, sizeof(FREQ), alphaCount, fpout);
 
 	ch = fgetc(fpin);
-	while(!feof(fpin)){
-		for(huffCodeIndex = 0; huf[indexOfch[ch]].huffCode[huffCodeIndex]; huffCodeIndex++){
-			if(huf[indexOfch[ch]].huffCode[huffCodeIndex] == '1'){
+	while (!feof(fpin))
+	{
+		for (huffCodeIndex = 0; huf[indexOfch[ch]].huffCode[huffCodeIndex]; huffCodeIndex++)
+		{
+			if (huf[indexOfch[ch]].huffCode[huffCodeIndex] == '1')
+			{
 				SET_BIT(hCode, bitIndex);
-			}else{
+			}
+			else
+			{
 				CLR_BIT(hCode, bitIndex);
 			}
-			if(++bitIndex == 8){
+			if (++bitIndex == 8)
+			{
 				bitIndex = 0;
 				fwrite(&hCode, sizeof(u8), 1, fpout);
 			}
@@ -130,10 +151,11 @@ void coddingFile(HUFF_TABLE *huf, FREQ *freq, int alphaCount, char *scFile, char
 		ch = fgetc(fpin);
 	}
 
-	if(bitIndex){
+	if (bitIndex)
+	{
 		fwrite(&hCode, sizeof(u8), 1, fpout);
 	}
-	
+
 	fseek(fpout, 0, SEEK_SET);
 
 	head.alphaCount = alphaCount;
@@ -146,19 +168,23 @@ void coddingFile(HUFF_TABLE *huf, FREQ *freq, int alphaCount, char *scFile, char
 	fclose(fpin);
 }
 
-void getTargetFileName(char *sourceFileName, char *targetFileName){
+void getTargetFileName(char *sourceFileName, char *targetFileName)
+{
 	int lastDotIndex = NOT_FOUND;
 	int i;
 	char sName[80];
 
 	strcpy(sName, sourceFileName);
-	for(i = 0; sName[i]; i++){
-		if(sName[i] == '.'){
+	for (i = 0; sName[i]; i++)
+	{
+		if (sName[i] == '.')
+		{
 			lastDotIndex = i;
 		}
 	}
 
-	if(lastDotIndex != NOT_FOUND){
+	if (lastDotIndex != NOT_FOUND)
+	{
 		sName[lastDotIndex] = 0;
 	}
 
@@ -166,46 +192,53 @@ void getTargetFileName(char *sourceFileName, char *targetFileName){
 	strcat(targetFileName, TARGET_FILE_EXT);
 }
 
-
-void makeHuffCode(HUFF_TABLE *huf, int root, char *str, int index){
-	if(huf[root].leftChild == -1){
+void makeHuffCode(HUFF_TABLE *huf, int root, char *str, int index)
+{
+	if (huf[root].leftChild == -1)
+	{
 		str[index] = 0;
 		strcpy(huf[root].huffCode, str);
-	}else{
+	}
+	else
+	{
 		str[index] = '0';
-		makeHuffCode(huf, huf[root].leftChild, str, index+1);
+		makeHuffCode(huf, huf[root].leftChild, str, index + 1);
 
 		str[index] = '1';
-		makeHuffCode(huf, huf[root].rightChild, str, index+1);
+		makeHuffCode(huf, huf[root].rightChild, str, index + 1);
 	}
 }
 
-void makeHuffTree(HUFF_TABLE *huf, int count){
+void makeHuffTree(HUFF_TABLE *huf, int count)
+{
 	short leftChildIndex;
 	short rightChildIndex;
 	int i;
 	int alphaCount = count;
 
-
-	for(i = 0; i < alphaCount-1; i++){
+	for (i = 0; i < alphaCount - 1; i++)
+	{
 		leftChildIndex = findMinFreqIndex(huf, count);
 		rightChildIndex = findMinFreqIndex(huf, count);
 		huf[count].freq.alpha = '#';
 		huf[count].freq.freq = huf[leftChildIndex].freq.freq + huf[rightChildIndex].freq.freq;
 		huf[count].leftChild = leftChildIndex;
 		huf[count].rightChild = rightChildIndex;
-		huf[count].isVisited =	FALSE;
+		huf[count].isVisited = FALSE;
 		huf[count].huffCode = NULL;
 		++count;
 	}
 }
 
-int findMinFreqIndex(HUFF_TABLE *huf, int count){
+int findMinFreqIndex(HUFF_TABLE *huf, int count)
+{
 	int minIndex = -1;
 	int i;
 
-	for(i = 0; i < count; i++){
-		if(huf[i].isVisited == FALSE && (minIndex == -1 || huf[minIndex].freq.freq > huf[i].freq.freq)){
+	for (i = 0; i < count; i++)
+	{
+		if (huf[i].isVisited == FALSE && (minIndex == -1 || huf[minIndex].freq.freq > huf[i].freq.freq))
+		{
 			minIndex = i;
 		}
 	}
@@ -215,20 +248,24 @@ int findMinFreqIndex(HUFF_TABLE *huf, int count){
 	return minIndex;
 }
 
-void showHuffTab(HUFF_TABLE *huf, int alphaCount){
-	int i = 0; 
+void showHuffTab(HUFF_TABLE *huf, int alphaCount)
+{
+	int i = 0;
 
-	printf("%4s %4s %4s %6s %6s %6s %s\n", "ÏÂ±ê", "×Ö·û", "Æµ¶È", "×óº¢×Ó", "ÓÒº¢×Ó", "·ÃÎÊ·ñ", "¹ş·òÂü±àÂë");
-	
-	for(i = 0; i < alphaCount; i++){
+	printf("%4s %4s %4s %6s %6s %6s %s\n", "ä¸‹æ ‡", "å­—ç¬¦", "é¢‘åº¦", "å·¦å­©å­", "å³å­©å­", "è®¿é—®å¦", "å“ˆå¤«æ›¼ç¼–ç ");
+
+	for (i = 0; i < alphaCount; i++)
+	{
 		printf("%4d %4c %4d %6d %6d %6d %s\n", i, huf[i].freq.alpha, huf[i].freq.freq, huf[i].leftChild, huf[i].rightChild, huf[i].isVisited, huf[i].huffCode);
 	}
 }
 
-void destoryHuffTab(HUFF_TABLE **huf, int alphaCount){
+void destoryHuffTab(HUFF_TABLE **huf, int alphaCount)
+{
 	int i;
 
-	for(i = 0; i < alphaCount; i++){
+	for (i = 0; i < alphaCount; i++)
+	{
 		free((*huf)[i].huffCode);
 	}
 
@@ -236,12 +273,14 @@ void destoryHuffTab(HUFF_TABLE **huf, int alphaCount){
 	*huf = NULL;
 }
 
-HUFF_TABLE *initHuffTab(FREQ *freq, int alphaCount){
+HUFF_TABLE *initHuffTab(FREQ *freq, int alphaCount)
+{
 	HUFF_TABLE *huf = NULL;
 	int i;
 
-	huf = (HUFF_TABLE *)calloc(sizeof(HUFF_TABLE), alphaCount*2 - 1);
-	for(i = 0; i < alphaCount; i++){
+	huf = (HUFF_TABLE *)calloc(sizeof(HUFF_TABLE), alphaCount * 2 - 1);
+	for (i = 0; i < alphaCount; i++)
+	{
 		huf[i].freq.alpha = freq[i].alpha;
 		huf[i].freq.freq = freq[i].freq;
 		huf[i].leftChild = huf[i].rightChild = -1;
@@ -252,16 +291,19 @@ HUFF_TABLE *initHuffTab(FREQ *freq, int alphaCount){
 	return huf;
 }
 
-void showFreq(const FREQ *freq, int alphaCount){
+void showFreq(const FREQ *freq, int alphaCount)
+{
 	int i;
 
-	puts("×Ö·û\tÆµ¶È");
-	for(i = 0; i < alphaCount; i++){
+	puts("å­—ç¬¦\té¢‘åº¦");
+	for (i = 0; i < alphaCount; i++)
+	{
 		printf("%c\t%d\n", freq[i].alpha, freq[i].freq);
 	}
 }
 
-int getFreq(char *fileName, FREQ **freq){
+int getFreq(char *fileName, FREQ **freq)
+{
 	int alpha[256] = {0};
 	int i;
 	int ch;
@@ -269,26 +311,31 @@ int getFreq(char *fileName, FREQ **freq){
 	int index = 0;
 	FILE *fp;
 
-	if((fp = fopen(fileName, "r")) == NULL)
+	if ((fp = fopen(fileName, "r")) == NULL)
 		return 0;
-	
+
 	ch = fgetc(fp);
-	while(!feof(fp)){
+	while (!feof(fp))
+	{
 		alpha[ch]++;
 		ch = fgetc(fp);
 	}
 	fclose(fp);
 
-	for(i = 0; i < 256; i++){
-		if(alpha[i]){
+	for (i = 0; i < 256; i++)
+	{
+		if (alpha[i])
+		{
 			alphaCount++;
 		}
 	}
 
 	*freq = (FREQ *)calloc(sizeof(FREQ), alphaCount);
 
-	for(i = 0; i < 256; i++){
-		if(alpha[i]){
+	for (i = 0; i < 256; i++)
+	{
+		if (alpha[i])
+		{
 			(*freq)[index].alpha = i;
 			(*freq)[index++].freq = alpha[i];
 		}
@@ -297,21 +344,26 @@ int getFreq(char *fileName, FREQ **freq){
 	return alphaCount;
 }
 
-void main(int argc, char **args){
+void main(int argc, char **args)
+{
 	FREQ *freq = NULL;
 	HUFF_TABLE *huf = NULL;
 	u8 alphaCount;
 	char *code;
 	char targetFileName[80] = "aaaaa.txt";
 
-	if(argc <= 1 || argc > 3){
-		puts("ÓÃ·¨: textFileHuff <Ô´ÎÄ¼şÃû> [Ä¿±êÎÄ¼şÃû]");
+	if (argc <= 1 || argc > 3)
+	{
+		puts("ç”¨æ³•: textFileHuff <æºæ–‡ä»¶å> [ç›®æ ‡æ–‡ä»¶å]");
 		return;
 	}
 
-	if(argc == 3){
+	if (argc == 3)
+	{
 		strcpy(targetFileName, args[2]);
-	}else{
+	}
+	else
+	{
 		getTargetFileName(args[1], targetFileName);
 	}
 
@@ -319,8 +371,8 @@ void main(int argc, char **args){
 	code = (char *)calloc(sizeof(char), alphaCount);
 	huf = initHuffTab(freq, alphaCount);
 	makeHuffTree(huf, alphaCount);
-	makeHuffCode(huf, 2*alphaCount - 2, code, 0);
-	showHuffTab(huf, 2*alphaCount - 1);
+	makeHuffCode(huf, 2 * alphaCount - 2, code, 0);
+	showHuffTab(huf, 2 * alphaCount - 1);
 	coddingFile(huf, freq, alphaCount, args[1], targetFileName);
 
 	destoryHuffTab(&huf, alphaCount);
