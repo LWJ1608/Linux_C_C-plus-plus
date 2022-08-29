9d1cf9ea92c5690e9edeaf57a28186d03861cba5
LWJ1608 <2194677100@qq.com>
2022-08-29 15:01:05 +0800

    push @2022年 08月 29日 星期一 15:01:05 CST

--- test01/set.cpp
+++ test01/set.cpp
@@ -2,5 +2,17 @@
  * @Author: lwj
  * @Date: 2022-08-29 14:59:43
  * @FilePath: /Linux_C_C-plus-plus/test01/set.cpp
- * @Description: 
+ * @Description: set
  **/
+#include<iostrea>
+#include<set>
+class compare
+{
+    public:
+};
+
+int main()
+{
+
+    return 0;
+}
