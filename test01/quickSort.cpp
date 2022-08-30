2179867c0ba4a0da444efcacb1fba4174c0fe959
LWJ1608 <2194677100@qq.com>
2022-08-30 10:31:04 +0800

    push @2022年 08月 30日 星期二 10:31:04 CST

--- test01/quickSort.cpp
+++ test01/quickSort.cpp
@@ -7,14 +7,23 @@
 
 #include <iostream>
 
-void printArr()
+void printArr(int *arr, const int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void quickSort()
 {
 }
 int main()
 {
     int arr1[] = {3, 5, 2, 6, 1, 3, 0};
-    int count = sizeof(arr) / sizeof(arr[1]);
+    int count = sizeof(arr1) / sizeof(arr1[1]);
 
-    printArr(arr, count);
+    printArr(arr1, count);
     return 0;
 }
