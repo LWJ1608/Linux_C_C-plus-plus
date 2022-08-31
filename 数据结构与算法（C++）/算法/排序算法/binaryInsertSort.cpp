becc94a42a0004e6370b375da4ace7e3223934a8
LWJ1608 <2194677100@qq.com>
2022-08-31 15:47:05 +0800

    push @2022年 08月 31日 星期三 15:47:04 CST

--- 数据结构与算法（C++）/算法/排序算法/binaryInsertSort.cpp
+++ 数据结构与算法（C++）/算法/排序算法/binaryInsertSort.cpp
@@ -2,5 +2,20 @@
  * @Author: lwj
  * @Date: 2022-08-31 15:45:22
  * @FilePath: /Linux_C_C-plus-plus/数据结构与算法（C++）/算法/排序算法/binaryInsertSort.cpp
- * @Description:
+ * @Description:折半插入排序
  **/
+#include <iostream>
+template <typename Type>
+void print(const Type &arr, int size) //打印数组
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+int main()
+{
+
+    return 0;
+}
