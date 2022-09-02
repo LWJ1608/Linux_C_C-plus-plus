0397a5f06fe091db0878fa5f5fd217b4ab9c6612
LWJ1608 <2194677100@qq.com>
2022-09-02 10:24:04 +0800

    push @2022年 09月 02日 星期五 10:24:04 CST

--- 数据结构与算法（C++）/数据结构/BinTree/struct版/binTree.h
+++ 数据结构与算法（C++）/数据结构/BinTree/struct版/binTree.h
@@ -232,7 +232,7 @@ void BinTree<Type>::postOrder(Node<Type> *t) const //递归后序遍历
 template <typename Type>
 void BinTree<Type>::preOrderWithStack() const //非递归前置遍历
 {
-    Node<Tyoe> *p = root;
+    Node<Type> *p = root;
     std::stack<Node<Type> *> s;
     while (!empty() || p)
     {
