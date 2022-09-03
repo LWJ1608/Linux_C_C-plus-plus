39722bbbfa4c6872d85e34a95358c5064360cfa4
LWJ1608 <2194677100@qq.com>
2022-09-03 09:07:05 +0800

    push @2022年 09月 03日 星期六 09:07:05 CST

--- 数据结构与算法（C++）/数据结构/BinTree/struct版/binTree.h
+++ 数据结构与算法（C++）/数据结构/BinTree/struct版/binTree.h
@@ -241,7 +241,7 @@ void BinTree<Type>::levelOrder(Node<Type> *t) const //层次遍历
         q.push(p);
         std::cout << p->data << " ";
         p = q.front();
-        p =
+        p = ;
     }
 }
 
