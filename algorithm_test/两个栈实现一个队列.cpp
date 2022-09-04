fb65a71af121329d6b0c6d5acdca870be5378d29
LWJ1608 <2194677100@qq.com>
2022-09-04 09:57:04 +0800

    push @2022年 09月 04日 星期日 09:57:04 CST

--- algorithm_test/两个栈实现一个队列.cpp
+++ algorithm_test/两个栈实现一个队列.cpp
@@ -4,3 +4,61 @@
  * @FilePath: /Linux_C_C-plus-plus/algorithm_test/两个栈实现一个队列.cpp
  * @Description:
  **/
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Test
+{
+public:
+    void push_(int data)
+    {
+        st1.push(data);
+    }
+    int pop_()
+    {
+        int tmp;
+
+        if (st2.empty())
+        {
+            while (!st1.empty())
+            {
+                int element = st1.top();
+                st1.pop();
+                st2.push(element);
+            }
+        }
+
+        if (!st2.empty())
+        {
+            tmp = st2.top();
+            st2.pop();
+        }
+
+        return tmp;
+    }
+
+private:
+    stack<int> st1;
+    stack<int> st2;
+};
+
+int main(void)
+{
+    Test t;
+    int num;
+
+    for (int i = 0; i < 7; i++)
+    {
+        t.push_(i);
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        num = t.pop_();
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
