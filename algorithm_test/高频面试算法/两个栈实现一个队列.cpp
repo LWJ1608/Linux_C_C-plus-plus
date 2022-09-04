b10fabfb652a3397af427ecf830030daea040d38
LWJ1608 <2194677100@qq.com>
2022-09-04 10:21:06 +0800

    push @2022年 09月 04日 星期日 10:21:06 CST

--- algorithm_test/高频面试算法/两个栈实现一个队列.cpp
+++ algorithm_test/高频面试算法/两个栈实现一个队列.cpp
@@ -4,43 +4,20 @@
  * @FilePath: /Linux_C_C-plus-plus/algorithm_test/两个栈实现一个队列.cpp
  * @Description:两个栈实现一个队列
  **/
+//思路：本题主要是利用栈的先进后出的特点，先把数据放到第一个栈中，
+//在出栈后再把第一个栈中的元素一一放入第二个栈中再出栈，这样就能完成一个先进先出的队列
 #include <iostream>
 #include <stack>
 using namespace std;
 
-class Test
+template <Typename T>
+class Queue
 {
-public:
-    void push_(int data)
-    {
-        st1.push(data);
-    }
-    int pop_()
-    {
-        int tmp;
-
-        if (st2.empty())
-        {
-            while (!st1.empty())
-            {
-                int element = st1.top();
-                st1.pop();
-                st2.push(element);
-            }
-        }
-
-        if (!st2.empty())
-        {
-            tmp = st2.top();
-            st2.pop();
-        }
-
-        return tmp;
-    }
-
 private:
-    stack<int> st1;
-    stack<int> st2;
+    stack<T> s2;
+
+    stack<T>
+        s2;
 };
 
 int main(void)
