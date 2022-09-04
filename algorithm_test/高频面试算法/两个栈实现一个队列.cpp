13697eadc313328d188232a3bb2826db5915146c
LWJ1608 <2194677100@qq.com>
2022-09-04 10:04:04 +0800

    push @2022年 09月 04日 星期日 10:04:04 CST

--- algorithm_test/高频面试算法/两个栈实现一个队列.cpp
+++ algorithm_test/高频面试算法/两个栈实现一个队列.cpp
@@ -2,7 +2,7 @@
  * @Author: lwj
  * @Date: 2022-09-03 15:35:29
  * @FilePath: /Linux_C_C-plus-plus/algorithm_test/两个栈实现一个队列.cpp
- * @Description:
+ * @Description:两个栈实现一个队列
  **/
 #include <iostream>
 #include <stack>
