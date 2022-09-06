decf0795911c4114f0961779acd1fb14fb375b87
LWJ1608 <2194677100@qq.com>
2022-09-06 15:09:05 +0800

    push @2022年 09月 06日 星期二 15:09:05 CST

--- 项目实战/基于加密的文件压缩解压缩 /compressHuf.c
+++ 项目实战/基于加密的文件压缩解压缩 /compressHuf.c
@@ -53,7 +53,7 @@ int getFreq(char *fileName, FREQ **freq);
 void showFreq(const FREQ *freq, int alphaCount);	   //输出字符和其相应的频度
 HUFF_TABLE *initHuffTab(FREQ *freq, int alphaCount);   //初始化哈夫曼表(叶子结点)
 void destoryHuffTab(HUFF_TABLE **huf, int alphaCount); //释放堆空间
-void showHuffTab(HUFF_TABLE *huf, int alphaCount);
+void showHuffTab(HUFF_TABLE *huf, int alphaCount);	   //输出哈夫曼树中所有的结点
 int findMinFreqIndex(HUFF_TABLE *huf, int count);
 void makeHuffTree(HUFF_TABLE *huf, int count);
 void makeHuffCode(HUFF_TABLE *huf, int root, char *str, int index);
