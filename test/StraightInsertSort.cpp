b2c3bbea4451f8261a2d36fbe08d4e88bd7ed79b
LWJ1608 <2194677100@qq.com>
2022-09-07 16:25:05 +0800

    push @2022年 09月 07日 星期三 16:25:05 CST

--- test/StraightInsertSort.cpp
+++ test/StraightInsertSort.cpp
@@ -1,6 +1,38 @@
 /**
  * @Author: lwj
- * @Date: 2022-09-07 15:43:31
- * @FilePath: /Linux_C_C-plus-plus/test/StraightInsertSort.cpp
+ * @Date: 2022-08-31 15:45:22
+ * @FilePath: /Linux_C_C-plus-plus/数据结构与算法（C++）/算法/排序算法/
  * @Description:
  **/
+#include <iostream>
+template <typename Type>
+void print(const Type &arr, int size) //打印数组
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+template <typename Type>
+void straightInsertSort(Type arr[], int size)
+{
+    int tmp;
+    for (int i = 1; i < count; i++)
+    {
+        tmp = arr[i];
+        for (int j = i - 1; j >= 0 && tmp < arr[j]; j--)
+        {
+            arr[j + 1] = arr[j];
+        }
+        arr[j] = tmp;
+    }
+}
+int main()
+{
+    int arr[] = {36, 80, 45, 66, 22, 9, 16, 36};
+    int count = sizeof(arr) / sizeof(arr[0]);
+    straightInsertSort(arr, count);
+    print(arr, count);
+    return 0;
+}
