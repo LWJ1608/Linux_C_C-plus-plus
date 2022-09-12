897768050b880ed0f851cc4033032a83cef1e368
LWJ1608 <2194677100@qq.com>
2022-09-12 15:36:04 +0800

    push @2022年 09月 12日 星期一 15:36:04 CST

--- test01/compressHuf.c
+++ test01/compressHuf.c
@@ -4,15 +4,17 @@
 #include <malloc.h>
 #include <string.h>
 
-
 #define TRUE 1
 #define FALSE 1
 
 // typedef unsigned char u8
 // typedef unsigned int u32
-// typedef 
+// typedef
+typedef struct Freq
+{
+    int 字符
+} Freq;
 
-void main(int argc, char **args){
-
-
+void main(int argc, char **args)
+{
 }
