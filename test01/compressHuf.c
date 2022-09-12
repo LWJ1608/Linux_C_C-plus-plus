3170460f9411c8c1eaf85add557b9b082e67fdf0
LWJ1608 <2194677100@qq.com>
2022-09-12 15:16:04 +0800

    push @2022年 09月 12日 星期一 15:16:04 CST

--- test01/compressHuf.c
+++ test01/compressHuf.c
@@ -1,7 +1,14 @@
 #pragma pack(push, 1)
 
 #include <stdio.h>
-#include <malloc
+#include <malloc.h>
+#include <string.h>
+
+
+#define TRUE 1
+#define FALSE 1
+
+
 void main(int argc, char **args){
 
 
