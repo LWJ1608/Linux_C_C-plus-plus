665976786b395af90f9693e82ef2e589ae7c9d85
LWJ1608 <2194677100@qq.com>
2022-09-16 09:37:04 +0800

    push @2022年 09月 16日 星期五 09:37:04 CST

--- test01/compressHuf.c
+++ test01/compressHuf.c
@@ -8,49 +8,45 @@
 #define FALSE 1
 
 // typedef unsigned char u8
-typedef unsigned int uint
+typedef unsigned int uint;
 
-typedef struct
+typedef struct Freq
 {
     int chars;     //字符种类
     int char_freq; //字符频度
 } Freq;
 typedef struct
 {
-    Freq freq;//字符种类及频度
-    int leftChild;//左孩子
-    int rightChild;//右孩子
-    int isVisited;//是否访问的标记
-    char* huffCode;//哈夫曼编码
-} Node;
+    Freq freq;      //字符种类及频度
+    int leftChild;  //左孩子
+    int rightChild; //右孩子
+    int isVisited;  //是否访问的标记
+    char *huffCode; //哈夫曼编码
+} HuffTable;
 typedef struct MechufHead
 {
-    char falg[6];//文件标识
-	int charCount;//字符种类个数 alphaCount*5 字符及其出现频度的数据块大小
+    char falg[6];  //文件标识
+    int charCount; //字符种类个数 alphaCount*5 字符及其出现频度的数据块大小
     int bitsCount
 };
-int getFreq(char* fileName,Freq **freq);
-//主要工作是记录字符种类个数，并把每一类存入堆空间中，最后返回字符种类个数
+int getFreq(char *fileName, Freq **freq);
 
-int getFreq(char* fileName,Freq **freq)
+//主要工作是记录字符种类个数，并把每一类存入堆空间中，最后返回字符种类个数
+int getFreq(char *fileName, Freq **freq)
 {
-    int str[256]={0};
-    int charCount=0;
+    int str[256] = {0};
+    int charCount = 0;
     int i;
     int ch;
     int index = 0;
-    FILE* fp;
-    fp = fopen("fileName",r);
-    if(fp == NULL)
+    FILE *fp;
+    fp = fopen("fileName", r);
+    if (fp == NULL)
     {
         return 0;
     }
-    
 }
 
-
-
-void main(int argc, char **args){
-    
-
+void main(int argc, char **args)
+{
 }
