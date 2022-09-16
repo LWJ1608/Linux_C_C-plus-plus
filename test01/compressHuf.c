a821cfc6f43986b3d9cbf9479e34d1b5d00f503b
LWJ1608 <2194677100@qq.com>
2022-09-16 09:20:04 +0800

    push @2022年 09月 16日 星期五 09:20:04 CST

--- test01/compressHuf.c
+++ test01/compressHuf.c
@@ -8,8 +8,8 @@
 #define FALSE 1
 
 // typedef unsigned char u8
-// typedef unsigned int u32
-// typedef
+typedef unsigned int uint
+
 typedef struct
 {
     int chars;     //字符种类
@@ -26,7 +26,9 @@ typedef struct
 typedef struct MechufHead
 {
     char falg[6];//文件标识
-    
+	 int charCount;//字符种类个数 alphaCount*5 字符及其出现频度的数据块大小
+   
+    int bitsCount
 };
 
 void main(int argc, char **args){
