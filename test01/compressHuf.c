7cb1d9ddc79bb327cd9506d6eded439052efae2d
LWJ1608 <2194677100@qq.com>
2022-09-17 10:33:04 +0800

    push @2022年 09月 17日 星期六 10:33:04 CST

--- test01/compressHuf.c
+++ test01/compressHuf.c
@@ -79,7 +79,6 @@ int getFreq(char *fileName, Freq **freq)
             (*freq)[index++].charFreq = str[i];
         }
     }
-    showChars(*freq, charCount);
     return charCount;
 }
 void showChars(const Freq *freq, int charCount) //输出字符和其相应的频度
