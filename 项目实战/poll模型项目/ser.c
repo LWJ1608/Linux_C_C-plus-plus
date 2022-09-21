57f90dfdca27085c7500eba0d73614d48996204f
LWJ1608 <2194677100@qq.com>
2022-09-21 18:19:04 +0800

    push @2022年 09月 21日 星期三 18:19:04 CST

--- 项目实战/poll模型项目/ser.c
+++ 项目实战/poll模型项目/ser.c
@@ -1,38 +1,42 @@
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<assert.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<errno.h>
-#include<string.h>
-#include<fcntl.h>
-#include<stdlib.h>
-#include<poll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <poll.h>
 
 #define _GNU_SOURCE 1
 
-#define USER_LIMIT  5
+#define USER_LIMIT 5
 #define BUFFER_SIZE 64
-#define FD_LIMIT    65535
+#define FD_LIMIT 65535
 
-#define POLLRDHUP   0x2000
+#define POLLRDHUP 0x2000
 
-struct client_data{
+struct client_data
+{
     struct sockaddr_in address;
     char *write_buf;
     char buf[BUFFER_SIZE];
 };
-int setnonblocking(int fd){
+int setnonblocking(int fd)
+{
     int old_option = fcntl(fd, F_GETFL);
     int new_option = old_option | O_NONBLOCK;
     fcntl(fd, F_SETFL, new_option);
     return old_option;
 }
 
-int main(int argc, char *argv[]){
-    if(argc <= 2){
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
         printf("usage: %s ip_address port_number\n", argv[0]);
         return 1;
     }
@@ -60,9 +64,10 @@ int main(int argc, char *argv[]){
 
     struct client_data *users = (struct client_data *)malloc(sizeof(struct client_data) * FD_LIMIT);
 
-    struct pollfd fds[USER_LIMIT+1];
+    struct pollfd fds[USER_LIMIT + 1];
     int user_counter = 0;
-    for(i = 0; i < USER_LIMIT; i++){
+    for (i = 0; i < USER_LIMIT; i++)
+    {
         fds[i].fd = -1;
         fds[i].events = 0;
     }
@@ -71,24 +76,30 @@ int main(int argc, char *argv[]){
     fds[0].events = POLLIN | POLLERR;
     fds[0].revents = 0;
 
-    while(1){
-        ret = poll(fds, user_counter+1, -1);
-        if(ret < 0){
+    while (1)
+    {
+        ret = poll(fds, user_counter + 1, -1);
+        if (ret < 0)
+        {
             printf("poll failure\n");
             break;
         }
 
-        for(i = 0; i < user_counter+1; i++){
-            if((fds[i].fd == listenfd) && (fds[i].revents & POLLIN)){
+        for (i = 0; i < user_counter + 1; i++)
+        {
+            if ((fds[i].fd == listenfd) && (fds[i].revents & POLLIN))
+            {
                 struct sockaddr_in client_address;
                 socklen_t client_addrlength = sizeof(client_address);
                 int connfd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlength);
-                if(connfd < 0){
+                if (connfd < 0)
+                {
                     printf("errno is: %d\n", errno);
                     continue;
                 }
 
-                if(user_counter >= USER_LIMIT){
+                if (user_counter >= USER_LIMIT)
+                {
                     const char *info = "too many users\n";
                     printf("%s", info);
                     send(connfd, info, strlen(info), 0);
@@ -102,39 +113,53 @@ int main(int argc, char *argv[]){
                 fds[user_counter].events = POLLIN | POLLRDHUP | POLLERR;
                 fds[user_counter].revents = 0;
                 printf("comes a new user, now have %d users\n", user_counter);
-            }else if(fds[i].revents & POLLERR){
+            }
+            else if (fds[i].revents & POLLERR)
+            {
                 printf("get an error from %d\n", fds[i].fd);
                 char errors[100];
                 memset(errors, '\0', 100);
                 socklen_t length = sizeof(errors);
-                if(getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR, &errors, &length) < 0){
+                if (getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR, &errors, &length) < 0)
+                {
                     printf("get socket option failed\n");
                 }
                 continue;
-            }else if(fds[i].revents & POLLRDHUP){
+            }
+            else if (fds[i].revents & POLLRDHUP)
+            {
                 users[fds[i].fd] = users[fds[user_counter].fd];
                 close(fds[i].fd);
                 fds[i] = fds[user_counter];
                 i--;
                 user_counter--;
                 printf("a client left\n");
-            }else if(fds[i].revents & POLLIN){
+            }
+            else if (fds[i].revents & POLLIN)
+            {
                 int connfd = fds[i].fd;
                 memset(users[connfd].buf, '\0', BUFFER_SIZE);
-                ret = recv(connfd, users[connfd].buf, BUFFER_SIZE-1, 0);
+                ret = recv(connfd, users[connfd].buf, BUFFER_SIZE - 1, 0);
                 printf("get %d bytes of client data %s from %d\n", ret, users[connfd].buf, connfd);
-                if(ret < 0){
-                    if(errno != EAGAIN){
+                if (ret < 0)
+                {
+                    if (errno != EAGAIN)
+                    {
                         close(connfd);
                         users[fds[i].fd] = users[fds[user_counter].fd];
                         i--;
                         user_counter--;
                     }
-                }else if(ret == 0){
-
-                }else{
-                    for(j = 1; j < user_counter; j++){
-                        if(fds[j].fd == connfd){
+                }
+                else if (ret == 0)
+                {
+                }
+                else
+                {
+                    for (j = 1; j < user_counter; j++)
+                    {
+                        if (fds[j].fd == connfd)
+                        {
                             continue;
                         }
                         fds[j].events |= ~POLLIN;
@@ -142,9 +167,12 @@ int main(int argc, char *argv[]){
                         users[fds[j].fd].write_buf = users[connfd].buf;
                     }
                 }
-            }else if(fds[i].revents & POLLOUT){
+            }
+            else if (fds[i].revents & POLLOUT)
+            {
                 int connfd = fds[i].fd;
-                if(!users[connfd].write_buf){
+                if (!users[connfd].write_buf)
+                {
                     continue;
                 }
 
@@ -153,7 +181,6 @@ int main(int argc, char *argv[]){
                 fds[i].events |= ~POLLOUT;
                 fds[i].events |= POLLIN;
             }
-
         }
     }
 
