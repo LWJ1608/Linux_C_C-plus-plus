439885f7913bc91c3f391526a5b8e1eff0ce30f0
LWJ1608 <2194677100@qq.com>
2022-10-01 14:35:04 +0800

    push @2022年 10月 01日 星期六 14:35:04 CST

---
+++ Linux/SystemProgramming/FunctionExercise/fork函数区分父子进程.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main()
+{
+	pid_t pid;
+	pid = fork();
+	if(pid < 0)
+	{//没有创建成功
+		perror("fork");
+	}
+	if(pid == 0)
+	{//子进程
+		while(1)
+		{
+			print("子进程");
+			sleep(1);
+		}
+	}
+	
+	else
+	{//父进程
+		while(1)
+		{
+			print("父进程");
+			sleep(1);
+		}
+		
+	return 0;
+}
