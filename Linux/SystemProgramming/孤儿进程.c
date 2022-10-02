aef09884226d4f260d2ead818510e66e243e2d65
LWJ1608 <2194677100@qq.com>
2022-10-02 16:23:04 +0800

    push @2022年 10月 02日 星期日 16:23:04 CST

---
+++ Linux/SystemProgramming/孤儿进程.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+/*
+ *孤儿进程是父进程已死而留下的子进程，该子进程会被1号进程收养，
+ *一号进程会回收它的资源，因此孤儿进程不会有危害。
+ * */
+
+int main()
+{	
+	int i;
+	pid_t pid = -1;
+	pid = fork();//创建子进程
+	if(pid == -1)
+	{
+		perror("fork");	
+		return 1;
+	}
+	//父进程
+	if(pid > 0)
+	{
+		printf("父进程睡一秒钟...");
+		sleep(1);
+		printf("父进程退出。");
+		exit(0);
+	}
+	else if(pid == 0)
+	{
+		for(i = 0;i < 5;i++)
+		{
+			printf("子进程在运行...%d\n",i);
+		｝			
+		printf("ppid=%d\n",fork());
+	}
+	return 0;		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
