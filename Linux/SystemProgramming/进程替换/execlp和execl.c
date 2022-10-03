1b65823c910706f9393d6ee000f58ddb8b92bf20
LWJ1608 <2194677100@qq.com>
2022-10-03 16:06:04 +0800

    push @2022年 10月 03日 星期一 16:06:04 CST

--- Linux/SystemProgramming/进程替换/execlp和execl.c
+++ Linux/SystemProgramming/进程替换/execlp和execl.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
 #include<unistd.h>
 //int execlp(const char* file,const char* arg, ... ,/* (char*)NULL*/)
+//int execl(const char* ,path,const char* arg, ... ,/* (char*)NULL*/)
 int main(void)
 {
-	printf("hello execlp()。");
+	//execlp:
 	//arg0 arg1 arg2 ...argn
 	//arg0为可执行文件，argn必须是NULL
-	//等价于ls -l /home
-	execlp("ls","ls","-l","/home",NULL);
+	//等价于ls -l /homie	
+	//execlp("ls","ls","-l","/home",NULL);
+	
+
+	//execl:
+	//arg0为可执行绝对文件路径或相对路径，第二个参数为可执行文件
+	//中间参数为可执行文件的参数
+	//最后一个参数为NULL
+	
 	printf("hello world!");	
+	execl("/bin/ls","ls","-l","/home",NULL);
 	
 	return 0;
 }
