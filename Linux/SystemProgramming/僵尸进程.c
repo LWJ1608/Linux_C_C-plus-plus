c46164443b595f297252d328f7d8b6b5a1ec943e
LWJ1608 <2194677100@qq.com>
2022-10-03 14:16:04 +0800

    push @2022年 10月 03日 星期一 14:16:04 CST

--- Linux/SystemProgramming/僵尸进程.c
+++ Linux/SystemProgramming/僵尸进程.c
@@ -5,6 +5,24 @@
 //僵尸进程：子进程结束后，父进程没有回收其资源
 int main(void)
 {
-	
+	int i = 0;
+	pid_t pid = -1;
+	pid = fork();
+	if(pid == -1)
+	{
+		perror("fork");
+		return 1;
+	}
+	//子进程
+	if(pid == 0)
+	{
+		for(i =0;i < 5;i++)
+		{
+			printf("子进程在工作......%d\n",i);
+			sleep(1);
+		}
+		printf("子进程结束了自己");
+		exit(0);
+	}
 	return 0;
 }
