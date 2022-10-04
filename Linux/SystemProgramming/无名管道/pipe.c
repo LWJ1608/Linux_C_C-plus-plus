45581d2e183c1e1c891c79fe37b5fea88068454b
LWJ1608 <2194677100@qq.com>
2022-10-04 22:12:04 +0800

    push @2022年 10月 04日 星期二 22:12:04 CST

---
+++ Linux/SystemProgramming/无名管道/pipe.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int fds[2];
+	int ret = -1;
+	
+	//创建一个无名管道
+	ret = pipe(fds);
+	if(ret == -1)
+	{
+		perror("pipe");
+		return 1;
+	}
+	//fds[0]用于读管道，fds[1]用于写
+	printf("fds[0]=%d,fds[1]=%d\n",fds[0],fds[1]);
+	
+	//关闭文件描述符
+	cloce(fds[0]);
+	cloce(fds[1]);
+	
+	return 0;
+}
