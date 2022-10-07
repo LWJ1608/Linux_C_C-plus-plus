0c31f9f9cf9dc340bab57c92e50de5154e56c63a
lwj <2194677100@qq.com>
2022-10-07 14:17:54 +0800

    push @Fri Oct  7 14:17:54 CST 2022

---
+++ Linux/SystemProgramming/有名管道实现简单聊天/takeA.c
@@ -0,0 +1,30 @@
+/*
+* @Author: lwj
+* @Date: 2022-10-07 14:01:13
+* @Mail: 2194677100@gmail.com 
+* @Description: 有名管道实现聊天
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+
+//先读后写
+//以只读的方式打开管道1
+//以只写的方式打开管道2
+int main(void)
+{
+	int fdr = -1;
+	int fdw = -1;
+	int ret = -1;
+	//以只读的方式打开管道1
+	ret = open("fifo1",)
+
+
+
+    return 0;
+}
