12ce1545b3ab719eaf7a1fabf91b2cf305a25765
lwj <2194677100@qq.com>
2022-10-07 15:20:34 +0800

    push @Fri Oct  7 15:20:34 CST 2022

---
+++ Linux/SystemProgramming/有名管道实现简单聊天/test.cpp
@@ -0,0 +1,16 @@
+/*
+* @Author: lwj
+* @Date: 2022-10-07 15:20:23
+* @Mail: 2194677100@gmail.com 
+* @Description:
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+
+
+    return 0;
+}
