9764b2ed47a578908a81467910301066e1943480
lwj <2194677100@qq.com>
2022-10-10 09:15:24 +0800

    push @Mon Oct 10 09:15:24 CST 2022

--- Linux/SystemProgramming/mmap.c
+++ Linux/SystemProgramming/mmap.c
@@ -23,6 +23,26 @@ int main(void)
 	int ret = -1;
 	void *addr = NULL;
 	//以读写的方式打开一个文件
+	fd = open("txt",O_RDWR)";
+	if(-1 = fd)
+	{
+		perror("open");
+		return 1;
+	}
+	//将文件映射到内存
+	addr = mmap(NULL,1024, PROT_READ | PROT_WRITE,MAP_SHARED,fd,0);
+	if(addr == MAP_FAILED)
+	{	
+		perror("mmap");
+		return 1;
+	}
+	//关闭文件
+	close(fd);
 
-    return 0;
+	//写文件
+	mmcpy(addr,"lwj is a ...",12);
+
+	//断开映射连接
+	munmap(addr,1024);
+	return 0;
 }
