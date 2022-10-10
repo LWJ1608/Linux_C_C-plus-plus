1384cb1cd70e18860d040d38d325f73a900bf3e0
lwj <2194677100@qq.com>
2022-10-10 08:59:15 +0800

    push @Mon Oct 10 08:59:15 CST 2022

---
+++ Linux/SystemProgramming/mmap.c
@@ -0,0 +1,28 @@
+/*
+* @Author: lwj
+* @Date: 2022-10-10 08:46:03
+* @Mail: h2194677100@gmail.com 
+* @Description:mmap函数的使用
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/mmap.h>
+
+
+
+int main(void)
+{
+	int fd = -1;
+	int ret = -1;
+	void *addr = NULL;
+	//以读写的方式打开一个文件
+
+    return 0;
+}
