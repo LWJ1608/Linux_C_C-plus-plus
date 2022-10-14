8964c0f326c8a05528995dee3efbd949ef00862a
lwj <2194677100@qq.com>
2022-10-14 20:39:05 +0800

    push @Fri Oct 14 20:39:05 CST 2022

---
+++ Linux/SystemProgramming/test.c
@@ -0,0 +1,17 @@
+/*
+* @Author: lwj
+* @Date: 2022-10-14 20:38:40
+* @Mail: 2194677100@gmail.com 
+* @Description:
+*/
+
+#include<stdio.h>
+
+
+int main(void)
+{
+
+         
+    return 0;int main(void)
+
+}
