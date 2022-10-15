eb92b704f3a42b43d6331bf5666a88e9bf09fc63
lwj <2194677100@qq.com>
2022-10-15 16:16:56 +0800

    push @Sat Oct 15 16:16:56 CST 2022

---
+++ Linux/SystemProgramming/线程/pthread_exit.c
@@ -0,0 +1,52 @@
+/*
+* @Author: lwj
+* @Date: 2022-10-15 14:58:39
+* @Mail: 2194677100@gmail.com 
+* @Description: 线程资源回收函数pthread_join
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include<pthread.h>
+
+/*
+    int pthread_join(pthread_t thread, void **retval);
+ */ 
+void* fun(void* arg)
+{
+    int i = 0;
+    for(i = 0;i < 5; i++)
+    {
+        printf("thread is running ...%d\n",i);
+        sleep(1);
+    }
+    return (void*)0x3;
+}
+
+int main(void)
+{
+    int red = -1;
+    void* tmp = NULL;
+    pthread_t tid = -1;
+    red =pthread_create(&tid,NULL,fun,NULL);
+    if(red != 0)
+    {
+        printf("thread creates false!\n");
+        return 1;
+    }
+    printf("main is runing ....\n");
+
+    //主线程等待子线程（回收线程资源），阻塞
+    red = -1;
+    red  = pthread_join(tid,&tmp);
+    if(red != 0)
+    {
+        printf("pthread_join false!");
+    }
+    printf("tmp:%d\n",tmp);
+    printf("main thread exit....\n");
+
+    return 0;
+}
