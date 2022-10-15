a6bb86ad5ba0d3838965647cfb34c517ddeabb2a
lwj <2194677100@qq.com>
2022-10-15 13:26:56 +0800

    push @Sat Oct 15 13:26:56 CST 2022

---
+++ Linux/SystemProgramming/线程/pthread_self.c
@@ -0,0 +1,21 @@
+/*
+* @Author: lwj
+* @Date: 2022-10-15 13:22:22
+* @Mail: 2194677100@gmail.com 
+* @Description:
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include<pthread.h>
+
+int main(void)
+{
+    pthread_t tid = 0;//获取当前线程号
+    tid = pthread_self();
+
+    printf("当前线程号为%d..",tid);
+    return 0;
+}
