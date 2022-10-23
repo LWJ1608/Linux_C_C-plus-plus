aa58cd238afd5036733a36b0707654caef0a88c2
lwj <2194677100@qq.com>
2022-10-23 20:38:55 +0800

    push @Sun Oct 23 20:38:55 CST 2022

---
+++ Linux/NetworkProgramming/套接字/ser1.c
@@ -0,0 +1,26 @@
+/*
+* @Author: lwj
+* @Date: 2022-10-23 20:23:37
+* @Mail: 2194677100@gmail.com 
+* @Description:
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<unistd.h>
+#include<arp/inet.h>
+#include<sys/socket.h>
+#include<string.h>
+
+int main(void)
+{
+    //创建套接字
+    //绑定
+    //监听
+    //提取
+    //读写
+    //关闭
+
+    return 0;
+}
