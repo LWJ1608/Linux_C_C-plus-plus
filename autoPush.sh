a9bcf7a277fed3bd4b4ecf36e5a706e4c8b5c035
lwj <2194677100@qq.com>
2022-10-23 20:17:36 +0800

    push @Sun Oct 23 20:17:36 CST 2022

--- autoPush.sh
+++ autoPush.sh
@@ -3,7 +3,7 @@
 push(){
     cd /home/lwj/Linux_C_C-plus-plus
     git pull git@github.com:LWJ1608/Linux_C_C-plus-plus.git
-    git pull 
+    git pull git@gitee.com:lwj08/Linux_C_C-plus-plus.git
     git add --all
     git commit -m "push @$(date)"
     git push origin master
