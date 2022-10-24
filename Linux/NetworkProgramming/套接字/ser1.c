cfa3ad6dd15026f2d0b2a8c1de0700fef3f8cc55
lwj <2194677100@qq.com>
2022-10-24 12:34:42 +0800

    push @Mon Oct 24 12:34:42 CST 2022

--- Linux/NetworkProgramming/套接字/ser1.c
+++ Linux/NetworkProgramming/套接字/ser1.c
@@ -1,26 +1,27 @@
 /*
-* @Author: lwj
-* @Date: 2022-10-23 20:23:37
-* @Mail: 2194677100@gmail.com 
-* @Description:
-*/
+ * @Author: lwj
+ * @Date: 2022-10-23 20:23:37
+ * @Mail: 2194677100@gmail.com
+ * @Description:
+ */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <string.h>
 
 int main(void)
 {
     //创建套接字
-    //绑定
-    //监听
-    //提取
-    //读写
-    //关闭
+    int lfd = socket(AF_INET)
+        //绑定
+        //监听
+        //提取
+        //读写
+        //关闭
 
-    return 0;
+        return 0;
 }
