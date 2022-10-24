cce99998db4971d316a371ed8d19d70a3008b7d0
lwj <2194677100@qq.com>
2022-10-24 15:22:39 +0800

    push @Mon Oct 24 15:22:39 CST 2022

--- Linux/NetworkProgramming/套接字/ser1.c
+++ Linux/NetworkProgramming/套接字/ser1.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <prerror.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -18,10 +18,27 @@ int main(void)
     //创建套接字
     int lfd = socket(AF_INET, SOCK_STREAM, 0);
     //绑定
-    struct sockaddr_in addr; 
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;//IP地址协议
+    addr.sin_port = htons(8000);//服务器端口号
+    inet_ptop(AF_INET,"192.168.43.128",addr.sin_addr.s_addr);
+    
+    int ret = bind(lfd,(struct sockaddr *)&addr,sizeof(addr));
+    if(ret < 0)
+    {
+        perror("");
+        exit(0);
+    }
     //监听
+    lisent(lfd,128);
     //提取
+    struct sockaddr_in ciladdr;//创建新的套接字
+    socklen_t len = sizeof(cliaddr);
+    int cfd = accept(lfd,(struct sockaddr*)&ciladdr,&len);
+    char ip[16]="";
+    printf("new client ip=%s, port=%d\n",inet_ntop(AF_INET,&cliaddr.sin_addr.s_addr,ip,16));
     //读写
+
     //关闭
 
     return 0;
