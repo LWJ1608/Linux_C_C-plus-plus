01c646f92bbdf41ae008a93fd362e2bd599714b8
lwj <2194677100@qq.com>
2022-10-25 20:04:18 +0800

    push @Tue Oct 25 20:04:18 CST 2022

--- Linux/NetworkProgramming/多路IO复用/select.c
+++ Linux/NetworkProgramming/多路IO复用/select.c
@@ -1,9 +1,9 @@
 /*
-* @Author: lwj
-* @Date: 2022-10-25 14:26:51
-* @Mail: 2194677100@gmail.com 
-* @Description:select()
-*/
+ * @Author: lwj
+ * @Date: 2022-10-25 14:26:51
+ * @Mail: 2194677100@gmail.com 
+ * @Description:select()
+ */
 
 #include<stdio.h>
 #include<sys/select.h>
@@ -25,52 +25,76 @@ int main(void)
     FD_ZERO(&oldset);
     FD_ZERO(&rset);
     FD_SET(lfd,&oldset);//将lfd放到oldset集合中
-    //while
+                        //while
     while(1)
     {
-       rset = oldset;//把oldset赋值给需要监听的rset
-        
-       int n = select(maxfd+1,&rset,NULL,NULL,NULL);
-       if(n < 0)
-       {
-           perror("");
-           break;
-       }
-       else if(n == 0)
-       {
+        rset = oldset;//把oldset赋值给需要监听的rset
+
+        int n = select(maxfd+1,&rset,NULL,NULL,NULL);
+        if(n < 0)
+        {
+            perror("");
+            break;
+        }
+        else if(n == 0)
+        {
             continue;//没有发生变化，继续监听
-       }
-       else //监听到文件描述符发生变化
-       {
-           struct sockaddr_in cliaddr;
-           socklen_t len = sizeof(cliaddr);
-           char ip[16] = "";
-           //提取新的连接
-           int cfd = Accept(lfd,(struct sockaddr*)&cliaddr,&len);
-           //输出客户端的信息
-           printf("new client ip=%s port=%d\n",inet_ntop(AF_INET,&cliaddr.sin_addr.s_addr,ip,16),
-                   ntohs(cliaddr.sin_port));
-           //将cfd放到oldset集合中，以下次监听
-           FD_SET(cfd,&oldset);
-           //更新maxfd
-           if(maxfd < cfd)
-           {
-               maxfd = cfd;
-           }
-           //如果只有lfd变化，continue 
-           if(--n == 0)
-           {
-               continue;
-           }
-       }
+        }
+        else //监听到文件描述符发生变化
+        {
+            struct sockaddr_in cliaddr;
+            socklen_t len = sizeof(cliaddr);
+            char ip[16] = "";
+            //提取新的连接
+            int cfd = Accept(lfd,(struct sockaddr*)&cliaddr,&len);
+            //输出客户端的信息
+            printf("new client ip=%s port=%d\n",inet_ntop(AF_INET,&cliaddr.sin_addr.s_addr,ip,16),
+                    ntohs(cliaddr.sin_port));
+            //将cfd放到oldset集合中，以下次监听
+            FD_SET(cfd,&oldset);
+            //更新maxfd
+            if(maxfd < cfd)
+            {
+                maxfd = cfd;
+            }
+            //如果只有lfd变化，continue 
+            if(--n == 0)
+            {
+                continue;
+            }
+        }
 
-       //cdf 遍历lfd之后的文件描述符是否在rset集合中，如果在则cfd变化
+        //cdf 遍历lfd之后的文件描述符是否在rset集合中，如果在则cfd变化
+        int i;
+        for(i =lfd;i <= maxfd;i++ )
+        {
+            char buf[1500] = "";
+            int ret = Read(i,buf,sizeof(buf));
+            if(ret < 0)
+            {
+                perror("");
+                close(i);                           
+                FD_CLE(i,&oldset);
+                continue;
+            }
+            else if(ret == 0)
+            {
+                printf("client close\n");
+                close(i);
+                FD_CLE(i,&oldset);
+            }
+            else
+            {
+                printf("%s\n",buf);
+                Write(i,buf,ret);
+            }
+
+        }
 
 
 
-        
-        
-        
+
+
     }
 
     return 0;
