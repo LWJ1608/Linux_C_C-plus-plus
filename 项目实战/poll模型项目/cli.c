bf9bb4094c936f924cf1b1aa8306e1c893fa7eeb
lwj <2194677100@qq.com>
2022-10-27 13:42:56 +0800

    push @Thu Oct 27 13:42:56 CST 2022

--- 项目实战/poll模型项目/cli.c
+++ 项目实战/poll模型项目/cli.c
@@ -1,14 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <assert.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
 #include <poll.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 #define _GNU_SOURCE 1
 #define POLLRDHUP 0x2000
