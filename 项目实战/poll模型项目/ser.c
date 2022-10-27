fc12584e5a2760141d9fd378e1d7f19bbb22efb0
lwj <2194677100@qq.com>
2022-10-27 13:24:57 +0800

    push @Thu Oct 27 13:24:57 CST 2022

--- 项目实战/poll模型项目/ser.c
+++ 项目实战/poll模型项目/ser.c
@@ -1,14 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <assert.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <poll.h>
 
 #define _GNU_SOURCE 1
