ce71577dffeff712dd0416f523fa0b964878047b
lwj <2194677100@qq.com>
2022-10-27 13:30:36 +0800

    push @Thu Oct 27 13:30:36 CST 2022

--- 项目实战/poll模型项目/cli.c
+++ 项目实战/poll模型项目/cli.c
@@ -1,26 +1,28 @@
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<assert.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<string.h>
-#include<stdlib.h>
-#include<poll.h>
-#include<fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <poll.h>
+#include <fcntl.h>
 
-#define _GNU_SOURCE     1
-#define POLLRDHUP   0x2000
-#define SPLICE_F_MOVE   1
-#define SPLICE_F_MORE   4
+#define _GNU_SOURCE 1
+#define POLLRDHUP 0x2000
+#define SPLICE_F_MOVE 1
+#define SPLICE_F_MORE 4
 #define BUFFER_SIZE 64
 
-int main(int argc, char *argv[]){
-    if(argc <= 2){ 
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
         printf("usage: %s ip_address port_number\n", argv[0]);
         return 1;
-    }   
+    }
     const char *ip = argv[1];
     int port = atoi(argv[2]);
 
@@ -32,7 +34,8 @@ int main(int argc, char *argv[]){
 
     int sockfd = socket(PF_INET, SOCK_STREAM, 0);
     assert(sockfd >= 0);
-    if(connect(sockfd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0){
+    if (connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
         printf("connection failed\n");
         close(sockfd);
         return 1;
@@ -51,23 +54,29 @@ int main(int argc, char *argv[]){
     int ret = pipe(pipefd);
     assert(ret != -1);
 
-    while(1){
+    while (1)
+    {
         ret = poll(fds, 2, -1);
-        if(ret < 0){
+        if (ret < 0)
+        {
             printf("poll failure\n");
             break;
         }
 
-        if(fds[1].revents & POLLRDHUP){
+        if (fds[1].revents & POLLRDHUP)
+        {
             printf("server close the connection\n");
             break;
-        }else if(fds[1].revents & POLLIN){
+        }
+        else if (fds[1].revents & POLLIN)
+        {
             memset(read_buf, '\0', BUFFER_SIZE);
-            recv(fds[1].fd, read_buf, BUFFER_SIZE-1, 0);
+            recv(fds[1].fd, read_buf, BUFFER_SIZE - 1, 0);
             printf("%s\n", read_buf);
         }
 
-        if(fds[0].revents & POLLIN){
+        if (fds[0].revents & POLLIN)
+        {
             ret = splice(0, NULL, pipefd[1], NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
             ret = splice(pipefd[0], NULL, sockfd, NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
         }
