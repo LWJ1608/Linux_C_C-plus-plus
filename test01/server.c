d988a1c645369efce2e1e143ff7ce6a5f3011551
lwj <2194677100@qq.com>
2022-10-28 15:52:38 +0800

    push @Fri Oct 28 15:52:38 CST 2022

--- test01/server.c
+++ test01/server.c
@@ -28,8 +28,8 @@ int main(int argc,char* argv)
         return 1;
     }
 
-    const char* ip = argv[1];//服务器ip地址
-    int port = atoi(argv[2]);//端口号
+    const char* ip = argv[1]; //服务器ip地址
+    int port = atoi(argv[2]); //端口号
     struct sockaddr_in address;
     bzero(&address,sizeof(address));
     address.sin_family = AF_INET;
