eddf78d449c2c850c294853d9603bba723156e76
lwj <2194677100@qq.com>
2022-10-28 18:37:58 +0800

    push @Fri Oct 28 18:37:58 CST 2022

--- test01/server.c
+++ test01/server.c
@@ -31,6 +31,9 @@ struct client_data
 int sernonblocking(int fd)
 {
     int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
 }
 int main(int argc, char *argv[])
 {
