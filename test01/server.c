4a122a1145aedd41472fddec2a293ed6968127a2
lwj <2194677100@qq.com>
2022-10-28 15:44:35 +0800

    push @Fri Oct 28 15:44:35 CST 2022

--- test01/server.c
+++ test01/server.c
@@ -18,7 +18,30 @@
 #include <fcntl.h>
 #include <poll.h>
 
-int main()
+
+
+int main(int argc,char* argv)
 {
-    //
+    if(argc <= 2)
+    {
+        printf("usage:%s ip_address port_number\n",argv[0]);
+        return 1;
+    }
+    const char* ip = argv[1];//服务器ip地址
+    int port = argv[2];//端口号
+    struct sockaddr_in address;
+    bzero(&address,sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    inet_pton(AF_INET,ip,&addres.sin_addr);
+
+    //创建套接字
+    int lisentfd = socket(AF_INET,SOCK_STREAM,0);
+    assert(listenfd >=0 );
+    //绑定
+    int ret = bind(lisentfd,(struct sockaddr*)&address,sizof(address));
+    assert(ret != -1);
+    //监听
+    ret = listen()
+    //while
 }
