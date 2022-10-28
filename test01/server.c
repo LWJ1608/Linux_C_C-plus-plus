90bced01ebeec6d9adc20ed4bdea3c0c3e949eaf
lwj <2194677100@qq.com>
2022-10-28 15:16:14 +0800

    push @Fri Oct 28 15:16:14 CST 2022

--- test01/server.c
+++ test01/server.c
@@ -4,3 +4,21 @@
  * @FilePath: /Linux_C_C-plus-plus/test01/server.c
  * @Description:
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+
+int main()
+{
+    //
+}
